9980d8a929596b5be4c2a68bdb709ae1ff110572
Elain Hu <besthynlove@gmail.com>
2021-12-11 17:06:40 -0500

    commit

--- src/com/company/lc_001_two_sum.java
+++ src/com/company/lc_001_two_sum.java
@@ -1,43 +1,24 @@
 package com.company;
 
 import java.util.HashMap; // import the HashMap class
+import java.util.Map;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 public class lc_001_two_sum {
 
     public static int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> run_map = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int x = nums[i];
-            if (run_map.containsKey(target - x)) {
-                return new int[]{run_map.get(target - x), i};
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i};
+
             }
-
-            run_map.put(x, i);
-            System.out.println(run_map);
+            map.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No two sum solution");
-    }
-    //Use the keySet() method if you only want the keys, and use the values() method if you only want the values:
-    public static void MapCat() {
-        HashMap<String, String> capitalCities = new HashMap<String, String>();
-        capitalCities.put("England", "London");
-        capitalCities.put("Germany", "Berlin");
-        capitalCities.put("Norway", "Oslo");
-        capitalCities.put("USA", "Washington DC");
+        return nums;
 
-        // Displaying the HashMap
-        System.out.println("Initial Mappings are: " + capitalCities);
-        // Displaying key/value pair
-        for (String i : capitalCities.keySet()) {
-            System.out.println("key: " + i + " value: " + capitalCities.get(i));
-        }
-
-        // Checking for the key_element "USA"
-        System.out.println(capitalCities.containsKey("USA"));
-        // Display value of key_element "USA"
-        System.out.println(capitalCities.get("USA"));
     }
 
     public static void main(String[] args){
@@ -45,9 +26,6 @@ public class lc_001_two_sum {
         int target = 5;
         int[] result = twoSum(intArray, target);
         System.out.println(Arrays.toString(result));
-        // test MapCat
-        MapCat();
+
     }
 }
-
-//my fking Java lc
