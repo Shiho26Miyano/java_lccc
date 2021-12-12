14d1a4124cc612f4a417a365e9fd5e96505345ce
Elain Hu <besthynlove@gmail.com>
2021-12-12 10:20:17 -0500

    commit

--- src/com/company/lc_001_two_sum.java
+++ src/com/company/lc_001_two_sum.java
@@ -6,19 +6,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 public class lc_001_two_sum {
-
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement)) {
                 return new int[] { map.get(complement), i};
-
             }
             map.put(nums[i], i);
         }
         return nums;
-
     }
 
     public static void main(String[] args){
@@ -26,6 +23,5 @@ public class lc_001_two_sum {
         int target = 5;
         int[] result = twoSum(intArray, target);
         System.out.println(Arrays.toString(result));
-
-    }
+            }
 }
