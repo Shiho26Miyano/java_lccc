ea9892823368a3db4886bab58ad6464f8691b2af
Elain Hu <besthynlove@gmail.com>
2021-12-14 11:10:44 -0500

    m

--- src/com/company/lc_14_Longest_common_Prefix.java
+++ src/com/company/lc_14_Longest_common_Prefix.java
@@ -1,25 +1,20 @@
 package com.company;
 
 public class lc_14_Longest_common_Prefix {
-
     public static String longestCommonPrefix(String[] strs) {
         if (strs.length == 0) return "";
         String prefix = strs[0];
         for (int i = 1; i < strs.length; i++)
             while (strs[i].indexOf(prefix) != 0) {
+
                 prefix = prefix.substring(0, prefix.length() - 1);
                 if (prefix.isEmpty()) return "";
             }
         return prefix;
     }
-
-
-
-
-
     public static void main(String[] args){
-        String[] x = new String[] {"flower","flow","flight"};
+        String[] x = new String[] {"aflower","filow","floight"};
         System.out.println(longestCommonPrefix(x));
-
+        //longestCommonPrefix(x);
     }
 }
