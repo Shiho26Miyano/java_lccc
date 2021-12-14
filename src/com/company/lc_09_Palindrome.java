cf7ce148ff42e039ae52d2c545bd49e2f9201d71
Elain Hu <besthynlove@gmail.com>
2021-12-14 08:10:57 -0500

    message

--- src/com/company/lc_09_Palindrome.java
+++ src/com/company/lc_09_Palindrome.java
@@ -4,10 +4,12 @@ public class lc_09_Palindrome {
     public static boolean isPalindrome(int num){
         if(num < 0) return  false;
         int reversed = 0, remainder, original = num;
+
         while(num != 0) {
             remainder = num % 10; // reversed integer is stored in variable
             reversed = reversed * 10 + remainder; //multiply reversed by 10 then add the remainder so it gets stored at next decimal place.
             num  /= 10;  //the last digit is removed from num after division by 10.
+            System.out.println(num);
         }
         // palindrome if original and reversed are equal
         return original == reversed;
