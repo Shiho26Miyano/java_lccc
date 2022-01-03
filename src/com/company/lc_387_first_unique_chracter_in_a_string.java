ece3716b72fca8f139706acfb315da52741fa4c4
Elain Hu <besthynlove@gmail.com>
2022-01-02 23:08:27 -0500

    m

---
+++ src/com/company/lc_387_first_unique_chracter_in_a_string.java
@@ -0,0 +1,29 @@
+package com.company;
+import java.util.*;
+
+public class lc_387_first_unique_chracter_in_a_string {
+    public static int firstUniqChar(String s) {
+        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+        int n = s.length();
+        // build hash map : character and how often it appears
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        // find the index
+        for (int i = 0; i < n; i++) {
+            if (count.get(s.charAt(i)) == 1)
+                return i;
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        String s = "cddddatfdsafffff";
+
+        int result = firstUniqChar(s);
+        System.out.println(result);
+    }
+}
