0e68cb13ea31c18d16fdab8f2597ca82c2f8cfa4
Elain Hu <besthynlove@gmail.com>
2022-01-04 11:03:02 -0500

    m

---
+++ src/com/company/lc_35_Search_Insert_Position.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.*;
+
+public class lc_35_Search_Insert_Position {
+    public static int searchInsert(int[] nums, int target) {
+        int pivot, left = 0, right = nums.length - 1;
+        while (left <= right) {
+            pivot = left + (right - left) / 2;
+            if (nums[pivot] == target) return pivot;
+            if (target < nums[pivot]) right = pivot - 1;
+            else left = pivot + 1;
+        }
+        return left;
+    }
+
+
+
+    public static void main(String[] args){
+        int[] intArray = new int[]{1,3,5,6};
+        int target = 4;
+        int result = searchInsert(intArray, target);
+        System.out.println(result);
+    }
+}
---
+++ src/com/company/lc_560_Subarray_Sum_Equals_K.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.*;
+
+public class lc_560_Subarray_Sum_Equals_K {
+
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap < Integer, Integer > map = new HashMap < > ();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            System.out.println(sum);
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            System.out.println(map);
+        }
+        System.out.println(map);
+        return count;
+    }
+
+    public static void main(String[] args){
+        int[] intArray = new int[]{1};
+        int target = 1;
+        int result = subarraySum(intArray, target);
+        System.out.println(result);
+    }
+}
