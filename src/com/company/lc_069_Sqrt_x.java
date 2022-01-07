16a8fd76eb0f2f47d14bf69dd0810631a7679f34
Elain Hu <besthynlove@gmail.com>
2022-01-07 14:53:10 -0500

    m

--- src/com/company/lc_069_Sqrt_x.java
+++ src/com/company/lc_069_Sqrt_x.java
@@ -10,7 +10,11 @@ public class lc_069_Sqrt_x {
         long num;
         int pivot, left = 2, right = x / 2;
         while (left <= right) {
+
+            System.out.println(left);
+            System.out.println(right);
             pivot = left + (right - left) / 2;
+            System.out.println(pivot);
             num = (long)pivot * pivot;
             if (num > x) right = pivot - 1;
             else if (num < x) left = pivot + 1;
@@ -23,7 +27,7 @@ public class lc_069_Sqrt_x {
 
     public static void main(String[] args){
         //int[] intArray = new int[]{2,7,11,15};
-        int target = 4;
+        int target = 32;
         int result = mySqrt(target);
         System.out.println(result);
     }
