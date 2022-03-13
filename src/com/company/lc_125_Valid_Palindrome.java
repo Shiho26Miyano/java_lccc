611819911448a89314595eff225655d5035dca32
Elain Hu <besthynlove@gmail.com>
2022-03-13 11:30:22 -0400

    add

---
+++ src/com/company/lc_125_Valid_Palindrome.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class lc_125_Valid_Palindrome {
+    public static boolean isPalindrome(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                System.out.println(s.charAt(i));
+                System.out.println("above i");
+                i++;
+            }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                System.out.println(s.charAt(j));
+                j--;
+            }
+
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+                return false;
+        }
+
+        return true;
+    }
+    public static void main(String[] args){
+        //String[] x = new String[] {"flower","filow","floight"};
+        String y = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(y));
+        //longestCommonPrefix(x);
+    }
+}
--- src/com/company/lc_14_Longest_common_Prefix.java
+++ src/com/company/lc_14_Longest_common_Prefix.java
@@ -2,18 +2,20 @@ package com.company;
 
 public class lc_14_Longest_common_Prefix {
     public static String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) return "";
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++)
-            while (strs[i].indexOf(prefix) != 0) {
-
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) return "";
+        if (strs == null || strs.length == 0) return "";
+        for (int i = 0; i < strs[0].length() ; i++){
+            char c = strs[0].charAt(i);
+            System.out.println(c);
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c)
+                    return strs[0].substring(0, i);
             }
-        return prefix;
+        }
+        return strs[0];
+
     }
     public static void main(String[] args){
-        String[] x = new String[] {"aflower","filow","floight"};
+        String[] x = new String[] {"flower","filow","floight"};
         System.out.println(longestCommonPrefix(x));
         //longestCommonPrefix(x);
     }
