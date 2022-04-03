873e61beb3399988bde6bcaa8cfeacca41c6cdb7
Elain Hu <besthynlove@gmail.com>
2022-04-03 18:19:42 -0400

    m

--- src/com/company/lc_189_rotate_array.java
+++ src/com/company/lc_189_rotate_array.java
@@ -9,11 +9,18 @@ public class lc_189_rotate_array {
         for (int i = 0; i < nums.length; i++) {
             a[(i + k) % nums.length] = nums[i];
         }
+
         for (int i = 0; i < nums.length; i++) {
             nums[i] = a[i];
         }
         return nums;
     }
+    public static int[] rotate2(int[] nums, int k) {
+
+
+    }
+
+
     public static void main(String[] args){
         //String s = "addccggazfeezddh";
         int[] nums2 = new int[]{2,9,3,6,8,7};
