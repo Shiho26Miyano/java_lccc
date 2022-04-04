2c85b313a1051bc9f47a66ca8ef5248ff9e75794
Elain Hu <besthynlove@gmail.com>
2022-04-04 19:17:36 -0400

    m

--- src/com/company/lc_15_3Sum.java
+++ src/com/company/lc_15_3Sum.java
@@ -4,26 +4,29 @@ public class lc_15_3Sum {
     public static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < nums.length && nums[i] <= 0; ++i)
-            if (i == 0 || nums[i - 1] != nums[i]) {
-                twoSumII(nums, i, res);
+        for (int i = 0; i < nums.length - 2; i++){
+            if (i > 0 && nums[i - 1] == nums[i]) {
+                continue;
             }
-        return res;
-    }
-    static void twoSumII(int[] nums, int i, List<List<Integer>> res) {
-        int lo = i + 1, hi = nums.length - 1;
-        while (lo < hi) {
-            int sum = nums[i] + nums[lo] + nums[hi];
-            if (sum < 0) {
-                ++lo;
-            } else if (sum > 0) {
-                --hi;
-            } else {
-                res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--]));
-                while (lo < hi && nums[lo] == nums[lo - 1])
-                    ++lo;
+            int lo = i + 1, hi = nums.length - 1;
+            while (lo < hi) {
+                int sum = nums[i] + nums[lo] + nums[hi];
+                if (sum < 0){
+                    lo++;
+                } else if (sum > 0){
+                    hi--;
+                } else{
+                    res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                    while (lo < hi && nums[lo] == nums[lo + 1]) lo++;
+                    while (lo < hi && nums[hi] == nums[hi - 1]) hi--;
+                    lo++;
+                    hi--;
+
+                }
             }
         }
+        return res;
+
     }
 
 
