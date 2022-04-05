f565f5961cce04af6a95f2656eb3505ea81feca1
Elain Hu <besthynlove@gmail.com>
2022-04-04 23:35:18 -0400

    m:

--- src/com/company/lc_238_Product_of_Array_Except_Self.java
+++ src/com/company/lc_238_Product_of_Array_Except_Self.java
@@ -3,25 +3,38 @@ import java.util.*;
 
 public class lc_238_Product_of_Array_Except_Self {
     public static int[] productExceptSelf(int[] nums) {
-        if (nums == null || nums.length <= 1) return new int[]{};
 
-        // check input for len = 1
-        int n = nums.length;
-        int[] rst = new int[n];
+        // The length of the input array
+        int length = nums.length;
 
-        for (int i = 0; i < n; i++) rst[i] = 1; // init
+        // Final answer array to be returned
+        int[] answer = new int[length];
 
-        int carry = nums[0]; // Bulid from left
-        for (int i = 1; i < n; i++) carry = multiply(rst, nums, i, carry);
+        // answer[i] contains the product of all the elements to the left
+        // Note: for the element at index '0', there are no elements to the left,
+        // so the answer[0] would be 1
+        answer[0] = 1;
+        for (int i = 1; i < length; i++) {
 
-        carry = nums[n - 1]; // Build from right
-        for (int i = n - 2; i >= 0; i--) carry = multiply(rst, nums, i, carry);
-        return rst;
-    }
+            // answer[i - 1] already contains the product of elements to the left of 'i - 1'
+            // Simply multiplying it with nums[i - 1] would give the product of all
+            // elements to the left of index 'i'
+            answer[i] = nums[i - 1] * answer[i - 1];
+        }
 
-    private static int multiply(int[] rst, int nums[], int i, int carry) {
-        rst[i] *= carry;
-        return carry * nums[i];
+        // R contains the product of all the elements to the right
+        // Note: for the element at index 'length - 1', there are no elements to the right,
+        // so the R would be 1
+        int R = 1;
+        for (int i = length - 1; i >= 0; i--) {
+
+            // For the index 'i', R would contain the
+            // product of all elements to the right. We update R accordingly
+            answer[i] = answer[i] * R;
+            R *= nums[i];
+        }
+
+        return answer;
     }
 
 
