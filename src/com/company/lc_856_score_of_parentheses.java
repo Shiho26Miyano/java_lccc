49179f62d79d625451ecfd68da2f47201d3a1d6b
Elain Hu <besthynlove@gmail.com>
2022-04-05 22:47:48 -0400

    m

--- src/com/company/lc_856_score_of_parentheses.java
+++ src/com/company/lc_856_score_of_parentheses.java
@@ -4,24 +4,24 @@ import java.util.*;
 public class lc_856_score_of_parentheses {
     public static int scoreOfParentheses(String S) {
         Stack<Integer> stack = new Stack();
-        stack.push(0); // The score of the current frame
+        int score = 0; // The score of the current frame
 
         for (char c: S.toCharArray()) {
-            if (c == '(')
-                stack.push(0);
+            if (c == '('){
+                stack.push(score);
+                score = 0;
+            }
             else {
-                int v = stack.pop();
-                int w = stack.pop();
-                stack.push(w + Math.max(2 * v, 1));
+                score = stack.pop() + Math.max(score*2, 1);
             }
         }
 
-        return stack.pop();
+        return score;
     }
     public static void main(String[] args){
         int[] intArray = new int[]{2,7,11,15};
         int target = 9;
-        String t = "()";
+        String t = "(()(()))";
         int result = scoreOfParentheses(t);
         //System.out.println(Arrays.toString(result));
         System.out.println(result);
