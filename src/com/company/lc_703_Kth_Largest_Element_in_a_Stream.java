21e7525a0edb3265f6035e9f0215d3469a784f2e
Elain Hu <besthynlove@gmail.com>
2022-04-17 23:24:43 -0400

    m

--- src/com/company/lc_703_Kth_Largest_Element_in_a_Stream.java
+++ src/com/company/lc_703_Kth_Largest_Element_in_a_Stream.java
@@ -1,41 +1,54 @@
 package com.company;
-
-import java.util.Arrays;
+import java.util.*;
 import java.util.PriorityQueue;
 public class lc_703_Kth_Largest_Element_in_a_Stream {
     private static int k;
-    private PriorityQueue<Integer> minHeap;
+    private PriorityQueue<Integer> miniheap;
 
-    public lc_703_Kth_Largest_Element_in_a_Stream(int k, int[] nums) {
+
+    public static PriorityQueue<Integer> lc_703_Kth_Largest_Element_in_a_Stream(int k, int[] nums) {
+
+        PriorityQueue<Integer> miniheap = new PriorityQueue<>();
+
+        for (int num: nums) {
+            miniheap.offer(num);
+        }
+
+        while (miniheap.size() > k) {
+            miniheap.poll();
+        }
+
+        return miniheap;
     }
 
-    public void lc_703_Kth_Largest_Element_in_a_Stream(int k, int[] nums) {
-        this.k = k;
-        for(int num: nums) {
-            minHeap.add(num);
+    public static int adda(int val, PriorityQueue<Integer> miniheap) {
+        miniheap.offer(val);
+        if (miniheap.size() > k) {
+            miniheap.poll();
         }
-        minHeap = new PriorityQueue<>((a , b) -> a - b);
-        while (minHeap.size() > k) {
-            minHeap.poll();
-        }
-    }
 
-    public int add(int val) {
-        minHeap.add(val);
-        if (minHeap.size() > k) {
-            minHeap.poll();
-        }
-        return minHeap.peek();
+        return miniheap.peek();
     }
 
 
-    public main(String[] args){
-        int[] nums = new int[]{2,7,11,15};
+    public static void main(String[] args){
+        int[] nums = new int[]{4, 5, 8, 2};
         int k = 3;
         //String t = "(()(()))";
-        lc_703_Kth_Largest_Element_in_a_Stream KthLargest = new lc_703_Kth_Largest_Element_in_a_Stream(k, nums);
-        int s = KthLargest.add(3);
-        //System.out.println(Arrays.toString(result));
-        System.out.println(s);
+        //first function
+        PriorityQueue<Integer> KthLargest = lc_703_Kth_Largest_Element_in_a_Stream(k ,nums);
+        System.out.println(KthLargest);
+        //second function
+        //int result = adda(3, KthLargest);
+        System.out.println(adda(3, KthLargest));
+        System.out.println(KthLargest);
+        System.out.println(adda(5, KthLargest));
+        System.out.println(KthLargest);
+        System.out.println(adda(10, KthLargest));
+        System.out.println(KthLargest);
+        System.out.println(adda(9, KthLargest));
+        System.out.println(KthLargest);
+        System.out.println(adda(15, KthLargest));
+        System.out.println(KthLargest);
     }
 }
