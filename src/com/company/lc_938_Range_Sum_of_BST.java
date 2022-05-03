fe97199663993b15d82b84bdfafd0a642c3a1f0a
Elain Hu <besthynlove@gmail.com>
2022-05-02 23:44:03 -0400

    m

---
+++ src/com/company/lc_938_Range_Sum_of_BST.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class lc_938_Range_Sum_of_BST {
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
