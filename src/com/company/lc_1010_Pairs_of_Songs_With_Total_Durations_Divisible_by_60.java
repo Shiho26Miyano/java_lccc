262f46bc764c7c6bd26ca5848ea0c8e60474203e
Elain Hu <besthynlove@gmail.com>
2022-05-06 14:27:00 -0400

    m

--- src/com/company/lc_1010_Pairs_of_Songs_With_Total_Durations_Divisible_by_60.java
+++ src/com/company/lc_1010_Pairs_of_Songs_With_Total_Durations_Divisible_by_60.java
@@ -3,16 +3,16 @@ package com.company;
 import java.util.Arrays;
 
 public class lc_1010_Pairs_of_Songs_With_Total_Durations_Divisible_by_60 {
-    public static int numPairsDivisibleBy60(int[] time) {
-        int count = 0, n = time.length;
-        for (int i = 0; i < n; i++) {
-            // j starts with i+1 so that i is always to the left of j
-            // to avoid repetitive counting
-            for (int j = i + 1; j < n; j++) {
-                if ((time[i] + time[j]) % 60 == 0) {
-                    count++;
-                }
+    public static  int numPairsDivisibleBy60(int[] time) {
+        int remainders[] = new int[60];
+        int count = 0;
+        for (int t: time) {
+            if (t % 60 == 0) { // check if a%60==0 && b%60==0
+                count += remainders[0];
+            } else { // check if a%60+b%60==60
+                count += remainders[60 - t % 60];
             }
+            remainders[t % 60]++; // remember to update the remainders
         }
         return count;
     }
