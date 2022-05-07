a4d1355e522b60f1a0551bd4a0d6066c120edb56
Elain Hu <besthynlove@gmail.com>
2022-05-07 17:04:09 -0400

    m

--- src/com/company/lc_1010_Pairs_of_Songs_With_Total_Durations_Divisible_by_60.java
+++ src/com/company/lc_1010_Pairs_of_Songs_With_Total_Durations_Divisible_by_60.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 
 public class lc_1010_Pairs_of_Songs_With_Total_Durations_Divisible_by_60 {
     public static  int numPairsDivisibleBy60(int[] time) {
-        int remainders[] = new int[60];
+        int[] remainders = new int[60];
+        System.out.println(Arrays.toString(remainders));
         int count = 0;
         for (int t: time) {
             if (t % 60 == 0) { // check if a%60==0 && b%60==0
+                System.out.println((remainders[0]));
                 count += remainders[0];
+
             } else { // check if a%60+b%60==60
+                System.out.println((60 - t % 60));
                 count += remainders[60 - t % 60];
             }
             remainders[t % 60]++; // remember to update the remainders
@@ -17,7 +21,7 @@ public class lc_1010_Pairs_of_Songs_With_Total_Durations_Divisible_by_60 {
         return count;
     }
     public static void main(String[] args){
-        int[] intArray = new int[]{30,20,150,100,40};
+        int[] intArray = new int[]{30,20,150,100,40, 60};
         int target = 5;
         int result = numPairsDivisibleBy60(intArray);
         System.out.println((result));
