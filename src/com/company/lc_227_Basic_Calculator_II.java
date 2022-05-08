8b27f84936b120c831899c5ce613d0a2f5122ace
Elain Hu <besthynlove@gmail.com>
2022-05-08 11:16:38 -0400

    m

---
+++ src/com/company/lc_227_Basic_Calculator_II.java
@@ -0,0 +1,46 @@
+package com.company;
+import java.util.*;
+
+public class lc_227_Basic_Calculator_II {
+    public static int calculate(String s) {
+
+        if (s == null || s.isEmpty()) return 0;
+        int len = s.length();
+        Stack<Integer> stack = new Stack<Integer>();
+        int currentNumber = 0;
+        char operation = '+';
+        for (int i = 0; i < len; i++) {
+            char currentChar = s.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == len - 1) {
+                if (operation == '-') {
+                    stack.push(-currentNumber);
+                }
+                else if (operation == '+') {
+                    stack.push(currentNumber);
+                }
+                else if (operation == '*') {
+                    stack.push(stack.pop() * currentNumber);
+                }
+                else if (operation == '/') {
+                    stack.push(stack.pop() / currentNumber);
+                }
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        //int[] intArray = new int[]{2,2,5,2,6,3,4,5,6,7};
+        String s = "3+2*2";
+        int result = calculate(s);
+        System.out.println(result);
+    }
+}
