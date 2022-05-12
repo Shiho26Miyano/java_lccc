2db1ce51faa0d0ef9ac159ddb9acb3c8ba4b8276
Elain Hu <besthynlove@gmail.com>
2022-05-11 22:17:42 -0400

    m

--- src/com/company/lc_442_Find_All_Duplicates_in_an_Array.java
+++ src/com/company/lc_442_Find_All_Duplicates_in_an_Array.java
@@ -5,20 +5,19 @@ public class lc_442_Find_All_Duplicates_in_an_Array {
 
     public static List<Integer> findDuplicates(int[] nums) {
         List<Integer> ans = new ArrayList<>();
-        Set<Integer> seen = new HashSet<>();
 
         for (int num : nums) {
-            if (seen.contains(num)) {
-                ans.add(num);
-            } else {
-                seen.add(num);
+            if (nums[Math.abs(num) - 1] < 0) { // seen before
+                ans.add(Math.abs(num));
             }
+            nums[Math.abs(num) - 1] *= -1;
         }
 
         return ans;
     }
+
     public static void main(String[] args){
-        int[] intArray = new int[]{4, 3, 2, 7, 8, 2, 3, 1};
+        int[] intArray = new int[]{4, 3, 7, 7, 8, 1, 3, 1};
         int target = 5;
         List<Integer> result =  findDuplicates(intArray);
         System.out.println(Arrays.toString(result.toArray()));
