fcb0f79830fc6f0e565edce5539863573005521f
Elain Hu <besthynlove@gmail.com>
2022-05-15 23:31:43 -0400

    m

---
+++ src/com/company/lc_151_Reverse_Words_in_a_String.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.*;
+
+public class lc_151_Reverse_Words_in_a_String {
+    public static String reverseWords(String s) {
+        // remove leading spaces
+        s = s.trim();
+        // split by multiple spaces
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+        Collections.reverse(wordList);
+        return String.join(" ", wordList);
+    }
+
+
+    public static void main(String[] args){
+        int[] intArray = new int[]{3,3,7,7,10,11,11};
+        int m = 3;
+        int n = 7;
+        String s = "the sky is blue";
+        //int target =5;
+        String result = reverseWords(s);
+        System.out.println(result);
+    }
+}
