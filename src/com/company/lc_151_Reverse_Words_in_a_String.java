b711a40078d2d931f72df53969aacbf579fea11f
Elain Hu <besthynlove@gmail.com>
2022-05-17 23:14:21 -0400

    m

--- src/com/company/lc_151_Reverse_Words_in_a_String.java
+++ src/com/company/lc_151_Reverse_Words_in_a_String.java
@@ -14,7 +14,7 @@ public class lc_151_Reverse_Words_in_a_String {
         int[] intArray = new int[]{3,3,7,7,10,11,11};
         int m = 3;
         int n = 7;
-        String s = "the sky is blue";
+        String s = "the sky is blueeee";
         //int target =5;
         String result = reverseWords(s);
         System.out.println(result);
