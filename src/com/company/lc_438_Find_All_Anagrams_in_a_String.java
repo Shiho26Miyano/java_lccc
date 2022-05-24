85d7df401c4230f682f7cbfe3a62cfab07a098ee
Elain Hu <besthynlove@gmail.com>
2022-05-24 00:09:30 -0400

    m

---
+++ src/com/company/lc_438_Find_All_Anagrams_in_a_String.java
@@ -0,0 +1,42 @@
+package com.company;
+import java.util.*;
+
+public class lc_438_Find_All_Anagrams_in_a_String {
+
+    public static List<Integer> findAnagrams(String s, String p) {
+        int ns = s.length(), np = p.length();
+        if (ns < np) return new ArrayList();
+
+        int [] pCount = new int[26];
+        int [] sCount = new int[26];
+        // build reference array using string p
+        for (char ch : p.toCharArray()) {
+            pCount[(int)(ch - 'a')]++;
+        }
+
+        List<Integer> output = new ArrayList();
+        // sliding window on the string s
+        for (int i = 0; i < ns; ++i) {
+            // add one more letter
+            // on the right side of the window
+            sCount[(int)(s.charAt(i) - 'a')]++;
+            // remove one letter
+            // from the left side of the window
+            if (i >= np) {
+                sCount[(int)(s.charAt(i - np) - 'a')]--;
+            }
+            // compare array in the sliding window
+            // with the reference array
+            if (Arrays.equals(pCount, sCount)) {
+                output.add(i - np + 1);
+            }
+        }
+        return output;
+    }
+    public static void main(String[] args){
+        int[] intArray = new int[]{3,3,7,7,10,11,11};
+        String s = "cbaebabacd";
+        String p = "abc";
+        System.out.println (findAnagrams(s, p));
+    }
+}
