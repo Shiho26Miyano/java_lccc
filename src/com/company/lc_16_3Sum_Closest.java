5452069a4a4613d96ac16a0041a7d4859432a271
Elain Hu <besthynlove@gmail.com>
2022-05-26 16:07:49 -0400

    m

--- src/com/company/lc_16_3Sum_Closest.java
+++ src/com/company/lc_16_3Sum_Closest.java
@@ -3,25 +3,27 @@ import java.util.*;
 public class lc_16_3Sum_Closest {
     public static int threeSumClosest(int[] nums, int target) {
         int diff = Integer.MAX_VALUE;
-        System.out.println (diff);
         int sz = nums.length;
         Arrays.sort(nums);
-        for (int i = 0; i < sz && diff != 0; ++i) {
+        for (int i = 0; i < sz; i++){
             int lo = i + 1;
             int hi = sz - 1;
-            while (lo < hi) {
+
+            while (lo < hi){
                 int sum = nums[i] + nums[lo] + nums[hi];
-                if (Math.abs(target - sum) < Math.abs(diff)) {
-                    diff = target - sum;
+                if(sum < target){
+                    lo++;
+                }else{
+                    hi--;
                 }
-                if (sum < target) {
-                    ++lo;
-                } else {
-                    --hi;
+                if(Math.abs(target - sum) < Math.abs(diff)){
+                    diff = target - sum;
+
                 }
             }
         }
         return target - diff;
+
     }
 
     public static void main(String[] args){
