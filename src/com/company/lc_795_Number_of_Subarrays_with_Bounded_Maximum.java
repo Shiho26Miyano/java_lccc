e0eb9ae85c4c7fe30bf2a796a1e662f8ee0e7cec
Elain Hu <besthynlove@gmail.com>
2022-05-30 19:11:21 -0400

    m

---
+++ src/com/company/lc_795_Number_of_Subarrays_with_Bounded_Maximum.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.util.*;
+
+public class lc_795_Number_of_Subarrays_with_Bounded_Maximum {
+    public static int numSubarrayBoundedMax(int[] A, int L, int R) {
+        return count(A, R) - count(A, L-1);
+    }
+
+    public static int count(int[] A, int bound) {
+        int ans = 0, cur = 0;
+        for (int x: A) {
+            cur = x <= bound ? cur + 1 : 0;
+            ans += cur;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args){
+        int[] intArray = new int[]{2,1,4,3};
+        String t = "abc";
+        String f ="ahbgdc";
+
+        System.out.println(count(intArray, 2));
+    }
+}
