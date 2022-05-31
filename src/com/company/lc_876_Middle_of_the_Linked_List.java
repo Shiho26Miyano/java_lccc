9fe301418d8c52c45c9a7b87c1490da398eb807a
Elain Hu <besthynlove@gmail.com>
2022-05-30 20:21:14 -0400

    m

---
+++ src/com/company/lc_876_Middle_of_the_Linked_List.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.*;
+public class lc_876_Middle_of_the_Linked_List {
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+}
+ class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
