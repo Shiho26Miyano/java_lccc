a24c02885e48f2ecf260fd82f623b4971076c7e0
Elain Hu <besthynlove@gmail.com>
2022-06-05 23:13:42 -0400

    m

---
+++ src/com/company/lc_716_Max_Stack.java
@@ -0,0 +1,40 @@
+package com.company;
+import java.util.*;
+
+public class lc_716_Max_Stack {
+    Stack<Integer> stack;
+    Stack<Integer> maxStack;
+
+    public void MaxStack() {
+        stack = new Stack();
+        maxStack = new Stack();
+    }
+
+    public void push(int x) {
+        int max = maxStack.isEmpty() ? x : maxStack.peek();
+        maxStack.push(max > x ? max : x);
+        stack.push(x);
+    }
+
+    public int pop() {
+        maxStack.pop();
+        return stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int peekMax() {
+        return maxStack.peek();
+    }
+
+    public int popMax() {
+        int max = peekMax();
+        Stack<Integer> buffer = new Stack();
+        while (top() != max) buffer.push(pop());
+        pop();
+        while (!buffer.isEmpty()) push(buffer.pop());
+        return max;
+    }
+}
