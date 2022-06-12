df6329bc0913ea124d682c2d237c9ceb13042e10
Elain Hu <besthynlove@gmail.com>
2022-06-12 11:56:38 -0400

    m

---
+++ src/com/company/lc_203_Remove_Linked_List_Elements.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class lc_203_Remove_Linked_List_Elements {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode sentinel = new ListNode(0);
+        sentinel.next = head;
+
+        ListNode prev = sentinel, curr = head;
+        while (curr != null) {
+            if (curr.val == val) prev.next = curr.next;
+            else prev = curr;
+            curr = curr.next;
+        }
+        return sentinel.next;
+
+    }
+
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
