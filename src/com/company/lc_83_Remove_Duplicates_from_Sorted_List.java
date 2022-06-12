8b25d5f03212b57e823291d720123b9554bfe169
Elain Hu <besthynlove@gmail.com>
2022-06-12 12:58:04 -0400

    m

--- src/com/company/lc_83_Remove_Duplicates_from_Sorted_List.java
+++ src/com/company/lc_83_Remove_Duplicates_from_Sorted_List.java
@@ -1,4 +1,24 @@
 package com.company;
 
 public class lc_83_Remove_Duplicates_from_Sorted_List {
+
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.next.val == current.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
 }
