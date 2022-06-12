f334e2bc03f1971169753f8f03def6e02e239087
Elain Hu <besthynlove@gmail.com>
2022-06-12 13:42:54 -0400

    m

---
+++ src/com/company/lc_1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class lc_1290_Convert_Binary_Number_in_a_Linked_List_to_Integer {
+    public int getDecimalValue(ListNode head) {
+        int num = head.val;
+        while (head.next != null) {
+            num = (num << 1) | head.next.val;
+            head = head.next;
+        }
+        return num;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
