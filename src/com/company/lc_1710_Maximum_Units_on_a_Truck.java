fbd4bde0c2437d96d9ada709005338a6fb45fda3
Elain Hu <besthynlove@gmail.com>
2022-06-20 12:25:32 -0400

    m

--- src/com/company/lc_1710_Maximum_Units_on_a_Truck.java
+++ src/com/company/lc_1710_Maximum_Units_on_a_Truck.java
@@ -4,11 +4,15 @@ import java.util.*;
 public class lc_1710_Maximum_Units_on_a_Truck {
 
     public static int maximumUnits(int[][] boxTypes, int truckSize) {
-        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b)->b[1] - a[1]);
-        queue.addAll(Arrays.asList(boxTypes));
+        PriorityQueue<int[]> myQueue = new PriorityQueue<>((a, b)->b[1] - a[1]);
+        for (int[] array: boxTypes){
+            System.out.println(Arrays.toString(array));
+        }
+
+        myQueue.addAll(Arrays.asList(boxTypes));
         int unitCount = 0;
-        while (!queue.isEmpty()) {
-            int[] top = queue.poll();
+        while (!myQueue.isEmpty()) {
+            int[] top = myQueue.poll();
             int boxCount = Math.min(truckSize, top[0]);
             unitCount += boxCount * top[1];
             truckSize -= boxCount;
@@ -16,6 +20,7 @@ public class lc_1710_Maximum_Units_on_a_Truck {
                 break;
         }
         return unitCount;
+    }
     }
     public static void main(String[] args){
         int[][] array = new int[][]{{1, 3}, {2, 2}, {3, 1}};
