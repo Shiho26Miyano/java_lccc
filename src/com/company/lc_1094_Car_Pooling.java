2bfc12690a7a687e6842eba3878ea77aca6fdf7b
Elain Hu <besthynlove@gmail.com>
2022-06-25 12:08:41 -0400

    m

--- src/com/company/lc_1094_Car_Pooling.java
+++ src/com/company/lc_1094_Car_Pooling.java
@@ -7,7 +7,11 @@ import java.util.*;
 public class lc_1094_Car_Pooling {
 
     public static boolean carPooling(int[][] trips, int capacity) {
+        // create a TreeMap timestamp, stored a list of k-v pair, with [k = start/end point, passengerCount]
         Map<Integer, Integer> timestamp = new TreeMap<>();
+        // sum startPassenger count based on start point;
+        // sum (minus) endPassenger count based on end point;
+        // fulfill the k-v pair timestamp
         for (int[] trip : trips) {
             int startPassenger = timestamp.getOrDefault(trip[1], 0) + trip[0];
             timestamp.put(trip[1], startPassenger);
@@ -18,6 +22,7 @@ public class lc_1094_Car_Pooling {
             System.out.println((timestamp));
             System.out.println("end");
         }
+        // loop sum up passenger count, check on each add/drop point whether the total count <= capacity 
         int usedCapacity = 0;
         for (int passengerChange : timestamp.values()) {
             System.out.println("timestamp values");
