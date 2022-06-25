5b08e470600f4c7cafed313d73b1a7e1f7a1809b
Elain Hu <besthynlove@gmail.com>
2022-06-25 11:43:23 -0400

    m

--- src/com/company/lc_1094_Car_Pooling.java
+++ src/com/company/lc_1094_Car_Pooling.java
@@ -1,6 +1,7 @@
 package com.company;
 import java.util.*;
-// trips = [[2,1,5],[3,3,7]], capacity = 4
+//[[8,2,3],[4,1,3],[1,3,6],[8,4,6],[4,4,8]]
+//        12
 // trips[i] = [numPassengersi, fromi, toi]
 
 public class lc_1094_Car_Pooling {
@@ -10,12 +11,17 @@ public class lc_1094_Car_Pooling {
         for (int[] trip : trips) {
             int startPassenger = timestamp.getOrDefault(trip[1], 0) + trip[0];
             timestamp.put(trip[1], startPassenger);
-
+            System.out.println((timestamp));
+            System.out.println("break");
             int endPassenger = timestamp.getOrDefault(trip[2], 0) - trip[0];
             timestamp.put(trip[2], endPassenger);
+            System.out.println((timestamp));
+            System.out.println("end");
         }
         int usedCapacity = 0;
         for (int passengerChange : timestamp.values()) {
+            System.out.println("timestamp values");
+            System.out.println(timestamp.values());
             usedCapacity += passengerChange;
             if (usedCapacity > capacity) {
                 return false;
@@ -25,7 +31,7 @@ public class lc_1094_Car_Pooling {
     }
 
     public static void main(String[] args){
-        int[][] array = new int[][]{{2, 1, 5}, {3, 3, 7}};
+        int[][] array = new int[][]{{8, 2, 3}, {4, 1, 3}, {1, 3, 6}, {8, 4, 6}, {4, 4, 8}};
         int target = 4;
         boolean result =  carPooling(array, target);
         System.out.println((result));
