4fe48a51879dbfc323a47086b60d535c95b14a3c
Elain Hu <besthynlove@gmail.com>
2022-06-26 19:27:35 -0400

    m

--- src/com/company/lc_692_Top_K_Frequent_Words.java
+++ src/com/company/lc_692_Top_K_Frequent_Words.java
@@ -2,29 +2,24 @@ package com.company;
 import java.util.*;
 public class lc_692_Top_K_Frequent_Words {
     public static List<String> topKFrequent(String[] words, int k) {
-        List<String> res = new LinkedList<>(); // add front from min heap
+        List<String> res = new LinkedList<>();
         Map<String, Integer> map = new HashMap<>();
-        for(int i = 0; i < words.length; i++){
-            map.put(words[i],map.getOrDefault(words[i], 0) + 1);
+        for (int i = 0; i < words.length; i++){
+            map.put(words[i], map.getOrDefault(words[i], 0) + 1);
         }
 
-        // max heap
-        PriorityQueue<Map.Entry<String, Integer>>pq = new PriorityQueue<>(
+        PriorityQueue<Map.Entry<String,Integer>> pq = new PriorityQueue<>(
                 (a,b) -> a.getValue() == b.getValue() ? b.getKey().compareTo(a.getKey()) :a.getValue() - b.getValue()
 
         );
-
         for (Map.Entry<String, Integer> entry: map.entrySet()){
-            pq.offer(entry);
-            if(pq.size()> k) pq.poll();
+            pq.add(entry);
+            if(pq.size()>k) pq.poll();
         }
-
-        System.out.print(Arrays.asList(pq));
 
         while(!pq.isEmpty()){
-            res.add(0, pq.poll().getKey()); // only record the name
+            res.add(0, pq.poll().getKey());
         }
-
         return res;
     }
     public static void main(String[] args){
