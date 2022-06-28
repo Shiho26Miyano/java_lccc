62ae09b175ff99a8bd4c2b61640438dfdb72ca84
Elain Hu <besthynlove@gmail.com>
2022-06-27 23:38:26 -0400

    m

--- src/com/company/lc_451_Sort_Characters_By_Frequency.java
+++ src/com/company/lc_451_Sort_Characters_By_Frequency.java
@@ -2,31 +2,28 @@ package com.company;
 import java.util.*;
 public class lc_451_Sort_Characters_By_Frequency {
     public static String frequencySort(String s) {
-        int[] map = new int[256];
-        for (char c: s.toCharArray()) map[c]++;
-        List<Character>[] bucket = new List[s.length() + 1];
-        for (int i=0; i<256; i++) {
-            int freq = map[i];
-            if (freq != 0) {
-                if (bucket[freq] == null) bucket[freq] = new LinkedList<>();
-                bucket[freq].add((char) i);
-            }
+
+        // Count up the occurances.
+        Map<Character, Integer> counts = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            counts.put(c, counts.getOrDefault(c, 0) + 1);
         }
 
+        // Make a list of the keys, sorted by frequency.
+        List<Character> characters = new ArrayList<>(counts.keySet());
+        Collections.sort(characters, (a, b) -> counts.get(b) - counts.get(a));
+
+        // Convert the counts into a string with a sb.
         StringBuilder sb = new StringBuilder();
-        for (int i=bucket.length-1; i>=0; i--) {
-            List<Character> list = bucket[i];
-            if (list != null) {
-                for (Character c: list) {
-                    char[] chars = new char[i];
-                    Arrays.fill(chars, c);
-                    sb.append(chars);
-                }
+        for (char c : characters) {
+            int copies = counts.get(c);
+            for (int i = 0; i < copies; i++) {
+                sb.append(c);
             }
         }
-
         return sb.toString();
     }
+
     public static void main(String[] args){
         int[] intArray = new int[]{2,2,5,5,5,5,2,6,3,4,2,5,6,7};
         int target = 2;
