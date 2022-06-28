b3e88cc67663d068fb2b895f01711734d1c28384
Elain Hu <besthynlove@gmail.com>
2022-06-27 23:24:47 -0400

    m

--- src/com/company/lc_347_Top_K_Frequent_Elements.java
+++ src/com/company/lc_347_Top_K_Frequent_Elements.java
@@ -10,36 +10,21 @@ import java.util.*;
 
 public class lc_347_Top_K_Frequent_Elements {
     public static int[] topKFrequent(int[] nums, int k) {
-        // O(1) time
-        if (k == nums.length) {
-            return nums;
-        }
-        Map<Integer, Integer> count = new HashMap<>();
-        for(int num : nums){
-            count.put(num, count.getOrDefault(num, 0) + 1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n: nums){
+            map.put(n, map.getOrDefault(n,0)+1);
         }
 
-        PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>(){
-            public int compare(int[] m, int[] n){
-                return m[1] - n[1];
-            }
-        });
-
-        for(Map.Entry<Integer, Integer> entry : count.entrySet()){
-            int num = entry.getKey(), cnt = entry.getValue();
-            if(queue.size() == k){
-                if(cnt > queue.peek()[1]){
-                    queue.poll();
-                    queue.offer(new int[]{num, cnt});
-                }
-            }else{
-                queue.offer(new int[]{num, cnt});
-            }
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a,b)->(a.getValue()-b.getValue()));
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            pq.add(entry);
+            if(pq.size()>k)
+                pq.poll();
         }
 
-        int[] res = new int[k];
-        for(int i = 0; i < k; i++){
-            res[i] = queue.poll()[0];
+        int[] res = new int[pq.size()];
+        for(int i = k - 1; i >= 0; --i) {
+            res[i] = pq.poll().getKey();
         }
         return res;
     }
