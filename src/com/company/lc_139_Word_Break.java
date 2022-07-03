e83027628e345cef33e0e8ecdf8f9845552441ce
Elain Hu <besthynlove@gmail.com>
2022-07-03 17:41:45 -0400

    m

--- src/com/company/lc_139_Word_Break.java
+++ src/com/company/lc_139_Word_Break.java
@@ -3,26 +3,24 @@ import java.util.*;
 
 public class lc_139_Word_Break {
     public static boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> wordDictSet = new HashSet<>(wordDict);
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[s.length()];
-        queue.add(0);
-        while (!queue.isEmpty()) {
-            int start = queue.remove();
-            if (visited[start]) {
-                continue;
-            }
-            for (int end = start + 1; end <= s.length(); end++) {
-                if (wordDictSet.contains(s.substring(start, end))) {
-                    queue.add(end);
-                    if (end == s.length()) {
-                        return true;
-                    }
+        int maxLen = Integer.MIN_VALUE;
+        for (String word : wordDict) {
+            maxLen = (word.length() > maxLen) ? word.length() : maxLen;
+        }
+
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int lastWordLength = 1; lastWordLength <= i && lastWordLength <= maxLen; lastWordLength++) {
+                if (dp[i - lastWordLength] && wordDict.contains(s.substring(i - lastWordLength, i))) {
+                    dp[i] = true;
+                    break;
                 }
             }
-            visited[start] = true;
         }
-        return false;
+        return dp[n];
+
     }
     public static void main(String[] args){
         int[] intArray = new int[]{1,1,0,1,1,1,0,0,1,1,1,1,1};
