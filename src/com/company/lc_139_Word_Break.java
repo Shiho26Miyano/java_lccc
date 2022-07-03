93eb67f43d4523ef8b962dd50e3b7b0149ec2a34
Elain Hu <besthynlove@gmail.com>
2022-07-03 17:51:28 -0400

    m

--- src/com/company/lc_139_Word_Break.java
+++ src/com/company/lc_139_Word_Break.java
@@ -3,25 +3,23 @@ import java.util.*;
 
 public class lc_139_Word_Break {
     public static boolean wordBreak(String s, List<String> wordDict) {
-        int maxLen = Integer.MIN_VALUE;
-        for (String word : wordDict) {
-            maxLen = (word.length() > maxLen) ? word.length() : maxLen;
-        }
-
         int n = s.length();
         boolean[] dp = new boolean[n + 1];
         dp[0] = true;
         for (int i = 1; i <= n; i++) {
-            for (int lastWordLength = 1; lastWordLength <= i && lastWordLength <= maxLen; lastWordLength++) {
-                if (dp[i - lastWordLength] && wordDict.contains(s.substring(i - lastWordLength, i))) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j]
+                        &&
+                        wordDict.contains(s.substring(j, i))) {
                     dp[i] = true;
                     break;
                 }
             }
         }
         return dp[n];
-
     }
+
+    
     public static void main(String[] args){
         int[] intArray = new int[]{1,1,0,1,1,1,0,0,1,1,1,1,1};
         String[] ls = new String[]{"leet", "code"};
