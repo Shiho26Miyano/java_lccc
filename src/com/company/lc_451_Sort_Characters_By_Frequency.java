67d69829599ba451d9b78e2988946747be7724c9
Elain Hu <besthynlove@gmail.com>
2022-07-05 01:31:11 -0400

    m

--- src/com/company/lc_451_Sort_Characters_By_Frequency.java
+++ src/com/company/lc_451_Sort_Characters_By_Frequency.java
@@ -1,20 +1,23 @@
 package com.company;
 import java.util.*;
 public class lc_451_Sort_Characters_By_Frequency {
-    public static String frequencySort(String s) {
+    public  static String frequencySort(String s) {
+        // create hashmap char: freq k-v pairs
         Map<Character, Integer> map = new HashMap();
         for (char c : s.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
+        // create empty decending Map.Entry List, sort the list
         List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
         Collections.sort(list, (o1, o2) -> (o2.getValue() - o1.getValue()));
-        StringBuilder stringBuilder = new StringBuilder();
+        //create StringBuilder, insert Map.Entry key to the sb
+        StringBuilder sb = new StringBuilder();
         for (Map.Entry<Character, Integer> entry : list) {
             for (int i = 0; i < entry.getValue(); i++) {
-                stringBuilder.append(entry.getKey());
+                sb.append(entry.getKey());
             }
         }
-        return stringBuilder.toString();
+        return sb.toString();
     }
 
     public static void main(String[] args){
