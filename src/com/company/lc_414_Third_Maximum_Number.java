406e957bf19041c22da7035cc4d1e15bd1cea12f
Elain Hu <besthynlove@gmail.com>
2022-07-05 22:50:20 -0400

    m

--- src/com/company/lc_414_Third_Maximum_Number.java
+++ src/com/company/lc_414_Third_Maximum_Number.java
@@ -5,24 +5,28 @@ public class lc_414_Third_Maximum_Number {
         long max1 = Long.MIN_VALUE;
         long max2 = Long.MIN_VALUE;
         long max3 = Long.MIN_VALUE;
-        for (int i : nums) {
+
+        // max1 is top max
+
+        for(int i : nums){
             max1 = Math.max(max1, i);
         }
-        for (int i : nums) {
-            if (i == max1) {
-                continue;
-            }
+
+        //if i = max1, continue
+        for (int i : nums){
+            if (i == max1) continue;
             max2 = Math.max(max2, i);
         }
-        for (int i : nums) {
-            if (i == max1 || i == max2) {
-                continue;
-            }
+
+        // if i = max2 or max1, continue
+        for (int i : nums){
+            if (i == max2 || i == max1) continue;
             max3 = Math.max(max3, i);
         }
-        return (int) (max3 == Long.MIN_VALUE ? max1 : max3);
+        return (int)(max3 == Long.MIN_VALUE? max1 : max3);
 
     }
+
     public static void main(String[] args){
         //String s = "addccggazfeezddh";
         int[] nums2 = new int[]{4,3,6,1,6,8,9,90};
