70f2350d2c8ef32458a3045fea6c530a89e3d578
Elain Hu <besthynlove@gmail.com>
2022-07-08 01:42:13 -0400

    m

--- src/com/company/lc_220_Contains_Duplicate_III.java
+++ src/com/company/lc_220_Contains_Duplicate_III.java
@@ -3,19 +3,22 @@ import java.util.*;
 
 public class lc_220_Contains_Duplicate_III {
     public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        TreeSet<Integer> set = new TreeSet<>();
+        /**case to Long to avoid Integer overflow.*/
+        TreeSet<Long> set = new TreeSet<>();
         for (int i = 0; i < nums.length; ++i) {
-            // Find the successor of current element
-            Integer s = set.ceiling(nums[i]);
-            if (s != null && (long) s <= nums[i] + t) return true;
+            Long s = set.ceiling((long) nums[i]);
+            if (s != null && s - nums[i] <= t) {
+                return true;
+            }
 
-            // Find the predecessor of current element
-            Integer g = set.floor(nums[i]);
-            if (g != null && nums[i] <= (long) g + t) return true;
+            Long g = set.floor((long) nums[i]);
+            if (g != null && nums[i] - g <= t) {
+                return true;
+            }
 
-            set.add(nums[i]);
+            set.add((long) nums[i]);
             if (set.size() > k) {
-                set.remove(nums[i - k]);
+                set.remove((long) nums[i - k]);
             }
         }
         return false;
