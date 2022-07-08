38ad29b03b70841e032643797a1f61732ac0a75c
Elain Hu <besthynlove@gmail.com>
2022-07-08 02:05:33 -0400

    m

---
+++ src/com/company/lc_101_Symmetric_Tree.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.util.*;
+
+public class lc_101_Symmetric_Tree {
+    public boolean isSymmetric(TreeNode root) {
+        return root==null || isSymmetricHelp(root.left, root.right);
+    }
+
+    private boolean isSymmetricHelp(TreeNode left, TreeNode right){
+        if(left==null || right==null)
+            return left==right;
+        if(left.val!=right.val)
+            return false;
+        return isSymmetricHelp(left.left, right.right) && isSymmetricHelp(left.right, right.left);
+    }
+
+
+    public class TreeNode {
+        public TreeNode left;
+        public TreeNode right;
+        int val;
+
+    }
+
+}
