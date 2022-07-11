0ac6f9ec0d62a25cf13ea63e5c26558a0b3e685f
Elain Hu <besthynlove@gmail.com>
2022-07-11 01:06:32 -0400

    m

---
+++ src/com/company/lc_104_Maximum_Depth_of_Binary_Tree.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class lc_104_Maximum_Depth_of_Binary_Tree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left_height = maxDepth(root.left);
+            int right_height = maxDepth(root.right);
+            return java.lang.Math.max(left_height, right_height) + 1;
+        }
+    }
+    public class TreeNode {
+        public TreeNode left;
+        public TreeNode right;
+        int val;
+
+    }
+}
