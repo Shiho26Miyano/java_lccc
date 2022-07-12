23ddc5e33dad6ec9bf61005eb6ad474e0899cd0f
Elain Hu <besthynlove@gmail.com>
2022-07-12 00:54:24 -0400

    m

---
+++ src/com/company/lc_100_Same_Tree.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class lc_100_Same_Tree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // p and q are both null
+        if (p == null && q == null) return true;
+        // one of p and q is null
+        if (q == null || p == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.right, q.right) &&
+                isSameTree(p.left, q.left);
+    }
+    public class TreeNode {
+        public TreeNode left;
+        public TreeNode right;
+        int val;
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
