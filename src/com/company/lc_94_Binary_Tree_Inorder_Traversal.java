09caee17d2e180b4b9e27d24129d8fd8bbcfcd25
Elain Hu <besthynlove@gmail.com>
2022-07-12 00:52:37 -0400

    m

---
+++ src/com/company/lc_94_Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,31 @@
+package com.company;
+import java.util.*;
+
+public class lc_94_Binary_Tree_Inorder_Traversal {
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public static void helper(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            helper(root.left, res);
+            res.add(root.val);
+            helper(root.right, res);
+        }
+    }
+
+
+    public class TreeNode {
+        public TreeNode left;
+        public TreeNode right;
+        int val;
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+
+    }
+}
