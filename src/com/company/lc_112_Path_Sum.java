51275091a64c0d9bf3697a4e4f47ace087a01a6d
Elain Hu <besthynlove@gmail.com>
2022-07-13 00:13:27 -0400

    m

---
+++ src/com/company/lc_112_Path_Sum.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class lc_112_Path_Sum {
+
+
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null)
+            return false;
+
+        sum -= root.val;
+        if ((root.left == null) && (root.right == null))
+            return (sum == 0);
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+    }
+
+
+    public class TreeNode {
+        public TreeNode left;
+        public TreeNode right;
+        int val;
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
