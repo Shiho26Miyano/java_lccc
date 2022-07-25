cf97a9e60b82cd82fbaf4409a0cc554d4a5dbd53
Elain Hu <besthynlove@gmail.com>
2022-07-25 00:43:20 -0400

    m

--- src/com/company/lc_217_Contains_Duplicate.java
+++ src/com/company/lc_217_Contains_Duplicate.java
@@ -1,15 +1,18 @@
 package com.company;
+import java.util.*;
 
 import java.util.Arrays;
 
 public class lc_217_Contains_Duplicate {
     public static boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 1; ++i) {
-            if (nums[i] == nums[i + 1]) return true;
+        Set<Integer> set = new HashSet<>(nums.length);
+        for (int x: nums) {
+            if (set.contains(x)) return true;
+            set.add(x);
         }
         return false;
     }
+
 
     public static void main(String[] args){
         int[] intArray = new int[]{1,2,3,1};
