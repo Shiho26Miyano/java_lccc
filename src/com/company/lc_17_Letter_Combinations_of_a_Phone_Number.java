a230adebdc6b9a718b53e282a2c5f2603544a038
Elain Hu <besthynlove@gmail.com>
2022-07-24 23:26:16 -0400

    m

--- src/com/company/lc_17_Letter_Combinations_of_a_Phone_Number.java
+++ src/com/company/lc_17_Letter_Combinations_of_a_Phone_Number.java
@@ -3,28 +3,41 @@ import java.util.*;
 
 public class lc_17_Letter_Combinations_of_a_Phone_Number {
     public static List<String> letterCombinations(String digits) {
+        // the purpose of the question is using stack, while length of element in the ans == i , element is poll() and element + mapping[i] all possibilities
         LinkedList<String> ans = new LinkedList<String>();
+        ans.add("");
         int L = digits.length();
         if (L == 0) return ans;
 
         String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        ans.add("");
+
         for(int i =0; i<digits.length();i++){
+            //loop each single number in the input string
             int x = Character.getNumericValue(digits.charAt(i));
+            // get the numeric value of each number
             while(ans.peek().length()==i){
-                String t = ans.remove();
+                //
+                String t = ans.poll();
                 for(char s : mapping[x].toCharArray())
                     ans.add(t+s);
+                //every combination.legnth() should be == di
             }
         }
         return ans;
     }
 
+
     public static void main(String[] args){
-        int[] intArray = new int[]{2,2,5,2,6,3,4,5,6,7};
+        String[] mapping = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         String strA= "23";
         List<String> result = letterCombinations(strA);
         System.out.println(result);
+        //practice
+        LinkedList<Character> ans = new LinkedList<Character>();
+        for (char s : mapping[2].toCharArray())
+            ans.add(s);
+        //System.out.println(ans);
+
     }
 
 }
