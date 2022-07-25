f83cc7506a3fd0d02ec77fe7c75fc7e04cd44a58
Elain Hu <besthynlove@gmail.com>
2022-07-25 00:48:45 -0400

    m

---
+++ src/com/company/lc_103_Binary_Tree_Zigzag_Level_Order_Traversal.java
@@ -0,0 +1,31 @@
+package com.company;
+import java.util.*;
+
+public class lc_103_Binary_Tree_Zigzag_Level_Order_Traversal {
+    protected void DFS(TreeNode node, int level, List<List<Integer>> results) {
+        if (level >= results.size()) {
+            LinkedList<Integer> newLevel = new LinkedList<Integer>();
+            newLevel.add(node.val);
+            results.add(newLevel);
+        } else {
+            if (level % 2 == 0)
+                results.get(level).add(node.val);
+            else
+                results.get(level).add(0, node.val);
+        }
+
+        if (node.left != null) DFS(node.left, level + 1, results);
+        if (node.right != null) DFS(node.right, level + 1, results);
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<List<Integer>>();
+        }
+        List<List<Integer>> results = new ArrayList<List<Integer>>();
+        DFS(root, 0, results);
+        return results;
+    }
+
+    
+}
