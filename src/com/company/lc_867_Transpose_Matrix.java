705e85ab04c59222391ab120dee462e97040cbbf
Elain Hu <besthynlove@gmail.com>
2022-07-28 23:08:35 -0400

    m

--- src/com/company/lc_867_Transpose_Matrix.java
+++ src/com/company/lc_867_Transpose_Matrix.java
@@ -4,6 +4,8 @@ import java.util.*;
 public class lc_867_Transpose_Matrix {
     public  static int[][] transpose(int[][] A) {
         int R = A.length, C = A[0].length;
+        System.out.println(R);
+        System.out.println(C);
         int[][] ans = new int[C][R];
         for (int r = 0; r < R; ++r)
             for (int c = 0; c < C; ++c) {
@@ -17,11 +19,12 @@ public class lc_867_Transpose_Matrix {
         int[][] matrix= {{1,2,3}, {4,5,6}, {7,8,9}};
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
+                //System.out.print(transpose(matrix)[i][j] + " ");
 
+            }
+            //System.out.println();
+        }
+        transpose(matrix);
     }
 
 }
