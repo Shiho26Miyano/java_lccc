e55e40aed3ccd9b81476a6c361e1a66fb25f8742
Elain Hu <besthynlove@gmail.com>
2022-07-31 23:40:21 -0400

    m

--- src/com/company/lc_486_Predict_the_Winner.java
+++ src/com/company/lc_486_Predict_the_Winner.java
@@ -8,14 +8,18 @@ public class lc_486_Predict_the_Winner {
     public static int winner(int[] nums, int s, int e, int turn) {
         if (s == e)
             return turn * nums[s];
+        System.out.println((turn));
         int a = turn * nums[s] + winner(nums, s + 1, e, -turn);
         int b = turn * nums[e] + winner(nums, s, e - 1, -turn);
         return turn * Math.max(turn * a, turn * b);
+
     }
     public static void main(String[] args){
-        int[] intArray = new int[]{1,5,2};
+        int[] intArray = new int[]{1,5,2,4};
         int target = 5;
         boolean result = PredictTheWinner(intArray);
         System.out.println((result));
     }
 }
+
+
