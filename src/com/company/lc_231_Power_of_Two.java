7e7b71fe6e03603ca829e89ca3732169ad272213
Elain Hu <besthynlove@gmail.com>
2022-08-02 08:38:49 -0400

    m

--- src/com/company/lc_231_Power_of_Two.java
+++ src/com/company/lc_231_Power_of_Two.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class lc_231_Power_of_Two {
     public static boolean isPowerOfTwo(int n) {
         if (n == 0) return false;
-        long x = (long) n;
-        return (x & (x - 1)) == 0;
+        while (n % 2 == 0) n /= 2;
+        return n == 1;
     }
     public static void main(String[] args){
         int[] intArray = new int[]{2,2,5,2,6,3,4,5,6,7};
