8e6479712afc2457b5419a844e54df5f0fd7347a
Elain Hu <besthynlove@gmail.com>
2022-08-05 08:34:32 -0400

    m

--- src/com/company/lc_697_Degree_of_an_Array.java
+++ src/com/company/lc_697_Degree_of_an_Array.java
@@ -3,29 +3,28 @@ import java.util.*;
 
 public class lc_697_Degree_of_an_Array {
     public static int findShortestSubArray(int[] nums) {
-        Map<Integer, Integer> left = new HashMap(),
-                right = new HashMap(), count = new HashMap();
+        HashMap<Integer, Integer> left = new HashMap<>(), right = new HashMap<>(), count = new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            int x = nums[i];
-            if (left.get(x) == null) left.put(x, i);
-            right.put(x, i);
-            count.put(x, count.getOrDefault(x, 0) + 1);
-            System.out.println(left);
-            System.out.println(right);
-            System.out.println(count);
-            System.out.println("that is it");
+        for (int i = 0; i < nums.length; i++){
+            if (left.get(nums[i]) == null) left.put(nums[i], i);
+            right.put(nums[i], i);
+            count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
         }
-
+        System.out.println((left));
+        System.out.println(right);
+        System.out.println((count));
         int ans = nums.length;
         int degree = Collections.max(count.values());
-        for (int x: count.keySet()) {
-            if (count.get(x) == degree) {
-                ans = Math.min(ans, right.get(x) - left.get(x) + 1);
+
+        for (int key : count.keySet()){
+            if (count. get(key) == degree){
+                ans = Math.min(ans, right.get(key) - left.get(key) + 1);
             }
         }
         return ans;
+
     }
+
     public static void main(String[] args){
         int[] intArray = new int[]{1,2,2,3,1};
         int target = 5;
