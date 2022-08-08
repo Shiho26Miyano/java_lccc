29700d313bb4fd24ace5f6e691cb82e76e842d09
Elain Hu <besthynlove@gmail.com>
2022-08-07 23:13:13 -0400

    m

---
+++ src/com/company/lc_804_Unique_Morse_Code_Words.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.util.*;
+public class lc_804_Unique_Morse_Code_Words {
+    public static int uniqueMorseRepresentations(String[] words) {
+        String[] MORSE = new String[]{".-","-...","-.-.","-..",".","..-.","--.",
+                "....","..",".---","-.-",".-..","--","-.",
+                "---",".--.","--.-",".-.","...","-","..-",
+                "...-",".--","-..-","-.--","--.."};
+
+        Set<String> seen = new HashSet();
+        for (String word: words) {
+            StringBuilder code = new StringBuilder();
+            for (char c: word.toCharArray())
+                code.append(MORSE[c - 'a']);
+            seen.add(code.toString());
+        }
+
+        return seen.size();
+    }
+    public static void main(String[] args){
+        String[] strArray = new String[]{"gin", "zen", "gig", "msg"};
+        int target = 5;
+        int result = uniqueMorseRepresentations(strArray);
+        System.out.println((result));
+    }
+}
