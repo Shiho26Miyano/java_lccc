0b310156364133dca34dc6718fd65b0a994f86d2
Elain Hu <besthynlove@gmail.com>
2022-08-08 14:39:07 -0400

    m

--- src/com/company/lc_290_Word_Pattern.java
+++ src/com/company/lc_290_Word_Pattern.java
@@ -2,25 +2,22 @@ package com.company;
 import java.util.*;;
 
 public class lc_290_Word_Pattern {
-    public static boolean wordPattern(String pattern, String s) {
-        HashMap map_index = new HashMap();
-        String[] words = s.split(" ");
+    public static boolean wordPattern(String pattern, String str) {
+        String[] arr = str.split(" ");
 
-        if (words.length != pattern.length())
+        //prevent out of boundary problem
+        if(arr.length != pattern.length())
             return false;
 
-        for (Integer i = 0; i < words.length; i++) {
+        HashMap<Character, String> map = new HashMap<Character, String>();
+        for(int i=0; i<pattern.length(); i++){
+
             char c = pattern.charAt(i);
-            String w = words[i];
-
-            if (!map_index.containsKey(c))
-                map_index.put(c, i);
-
-            if (!map_index.containsKey(w))
-                map_index.put(w, i);
-
-            if (map_index.get(c) != map_index.get(w))
-                return false;
+            if(map.containsKey(c)){
+                String value = map.get(c);
+                if(!value.equals(arr[i]))  return false;
+            }else if (map.containsValue(arr[i])) return false;
+            map.put(c, arr[i]);
         }
 
         return true;
