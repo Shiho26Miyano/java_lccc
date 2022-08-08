2d5b8e1eded629a9a76fd7b90337ffc9cd552d90
Elain Hu <besthynlove@gmail.com>
2022-08-08 08:45:01 -0400

    m

--- src/com/company/lc_290_Word_Pattern.java
+++ src/com/company/lc_290_Word_Pattern.java
@@ -3,29 +3,24 @@ import java.util.*;;
 
 public class lc_290_Word_Pattern {
     public static boolean wordPattern(String pattern, String s) {
-        HashMap <Character, String> map_char = new HashMap();
-        HashMap <String, Character> map_word = new HashMap();
+        HashMap map_index = new HashMap();
         String[] words = s.split(" ");
 
         if (words.length != pattern.length())
             return false;
 
-        for (int i = 0; i < words.length; i++) {
+        for (Integer i = 0; i < words.length; i++) {
             char c = pattern.charAt(i);
             String w = words[i];
-            if (!map_char.containsKey(c)) {
-                if (map_word.containsKey(w)) {
-                    return false;
-                } else {
-                    map_char.put(c, w);
-                    map_word.put(w, c);
-                }
 
-            } else {
-                String mapped_word = map_char.get(c);
-                if (!mapped_word.equals(w))
-                    return false;
-            }
+            if (!map_index.containsKey(c))
+                map_index.put(c, i);
+
+            if (!map_index.containsKey(w))
+                map_index.put(w, i);
+
+            if (map_index.get(c) != map_index.get(w))
+                return false;
         }
 
         return true;
