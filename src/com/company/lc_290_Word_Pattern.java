b59153597063eb3727360969b30632c2f85b7d55
Elain Hu <besthynlove@gmail.com>
2022-08-08 15:08:09 -0400

    m

--- src/com/company/lc_290_Word_Pattern.java
+++ src/com/company/lc_290_Word_Pattern.java
@@ -3,25 +3,24 @@ import java.util.*;;
 
 public class lc_290_Word_Pattern {
     public static boolean wordPattern(String pattern, String str) {
-        String[] arr = str.split(" ");
+        String[] words = str.trim().split(" ");
+        if (pattern.length() != words.length) return false;
+        Map<Character, String> map = new HashMap<>();
+        int N = pattern.length();
+        for (int i=0; i<N; i++) {
+            String w = words[i];
+            Character ch = pattern.charAt(i);
+            if (map.containsKey(ch)) {
+                if (!map.get(ch).equals(w)) return false;
+            } else if (map.values().contains(w)) return false;
 
-        //prevent out of boundary problem
-        if(arr.length != pattern.length())
-            return false;
 
-        HashMap<Character, String> map = new HashMap<Character, String>();
-        for(int i=0; i<pattern.length(); i++){
-
-            char c = pattern.charAt(i);
-            if(map.containsKey(c)){
-                String value = map.get(c);
-                if(!value.equals(arr[i]))  return false;
-            }else if (map.containsValue(arr[i])) return false;
-            map.put(c, arr[i]);
+            map.put(ch, w);
         }
-
         return true;
     }
+
+
     public static void main(String[] args){
         String[] strArray = new String[]{"gin", "zen", "gig", "msg"};
         int target = 5;
