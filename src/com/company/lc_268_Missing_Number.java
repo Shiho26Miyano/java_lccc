a61414a3df5ff75326c49dde6a6bde36252dce9f
Elain Hu <besthynlove@gmail.com>
2022-08-11 00:51:43 -0400

    m

--- src/com/company/lc_268_Missing_Number.java
+++ src/com/company/lc_268_Missing_Number.java
@@ -20,25 +20,21 @@ public class lc_268_Missing_Number {
     public static int missingNumber2(int[] nums) {
         Arrays.sort(nums);
 
-        // Ensure that n is at the last index
-        if (nums[nums.length - 1] != nums.length) {
+        if (nums[nums.length-1] != nums.length) {
             return nums.length;
         }
-        // Ensure that 0 is at the first index
+
         else if (nums[0] != 0) {
             return 0;
         }
 
-        // If we get here, then the missing number is on the range (0, n)
-        for (int i = 1; i < nums.length; i++) {
-            int expectedNum = nums[i - 1] + 1;
-            if (nums[i] != expectedNum) {
-                return expectedNum;
-            }
+        for (int i = 0; i < nums.length + 1; i++) {
+            if (nums[i] != i) return nums[i] - 1;
+
         }
 
-        // Array was not missing any numbers
         return -1;
+
     }
 
     public static void main(String[] args) {
