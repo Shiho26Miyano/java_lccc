fd7c079e19afe52ae02adc13e7c29fecf2884f4b
Elain Hu <besthynlove@gmail.com>
2022-08-14 11:10:29 -0400

    m

--- src/com/company/lc_412_Fizz_Buzz.java
+++ src/com/company/lc_412_Fizz_Buzz.java
@@ -3,45 +3,21 @@ import java.util.*;;
 
 public class lc_412_Fizz_Buzz {
     public static List<String> fizzBuzz(int n) {
-
-        // ans list
-        List<String> ans = new ArrayList<String>();
-
-        // Hash map to store all fizzbuzz mappings.
-        HashMap<Integer, String> fizzBizzDict =
-                new HashMap<Integer, String>() {
-                    {
-                        put(3, "Fizz");
-                        put(5, "Buzz");
-                    }
-                };
-        System.out.println(fizzBizzDict);
-        // List of divisors which we will iterate over.
-        List<Integer> divisors = new ArrayList<>(Arrays.asList(3, 5));
-
-        for (int num = 1; num <= n; num++) {
-
-            String numAnsStr = "";
-
-            for (Integer key : divisors) {
-
-                // If the num is divisible by key,
-                // then add the corresponding string mapping to current numAnsStr
-                if (num % key == 0) {
-                    numAnsStr += fizzBizzDict.get(key);
-                }
+        List<String> res = new ArrayList<>();
+        for (int i=1; i<=n; i++) {
+            boolean isFizz = i % 3 == 0;
+            boolean isBuzz = i % 5 == 0;
+            if (isFizz && isBuzz) {
+                res.add("FizzBuzz");
+            } else if (isFizz) {
+                res.add("Fizz");
+            } else if (isBuzz) {
+                res.add("Buzz");
+            } else {
+                res.add(Integer.toString(i));
             }
-
-            if (numAnsStr.equals("")) {
-                // Not divisible by 3 or 5, add the number
-                numAnsStr += Integer.toString(num);
-            }
-
-            // Append the current answer str to the ans list
-            ans.add(numAnsStr);
         }
-
-        return ans;
+        return res;
     }
     public static void main(String[] args){
         int[][] intArray = new int[][]{{0, 30}, {5, 10}, {15, 20}};
