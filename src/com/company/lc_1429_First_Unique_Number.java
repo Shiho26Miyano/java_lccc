6fe52329b53b31094b3ba6a503208ac31de867c4
Elain Hu <besthynlove@gmail.com>
2022-08-14 15:49:52 -0400

    m

--- src/com/company/lc_1429_First_Unique_Number.java
+++ src/com/company/lc_1429_First_Unique_Number.java
@@ -2,30 +2,32 @@ package com.company;
 import java.util.*;;
 
 public class lc_1429_First_Unique_Number {
-    HashMap<Integer, Integer> recorder = new HashMap<Integer, Integer>();
+    HashMap<Integer, Integer> map = new HashMap<>();
     Queue<Integer> data = new LinkedList();
+
     public void FirstUnique(int[] nums) {
-        for(int num : nums){
+        for (int num: nums){
             add(num);
         }
+
     }
 
     public int showFirstUnique() {
         while(!data.isEmpty()){
-            int cur = data.peek();
-            if(recorder.get(cur) == 1){
-                return cur;
-            }
+            int ans = data.peek();
+            if (map.get(ans) == 1) return ans;
             data.poll();
         }
         return -1;
+
     }
 
     public void add(int value) {
-        int f = recorder.getOrDefault(value, 0);
-        recorder.put(value, f + 1);
-        data.offer(value);
+        map.put(value, map.getOrDefault(value, 0) + 1);
+        data.add(value);
+
     }
+
 
 
 }
