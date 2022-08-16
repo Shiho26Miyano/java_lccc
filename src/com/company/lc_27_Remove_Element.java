bfd229f4a4651ed8044b3f955f5f2c5eb9342aef
Elain Hu <besthynlove@gmail.com>
2022-08-15 23:05:17 -0400

    m

--- src/com/company/lc_27_Remove_Element.java
+++ src/com/company/lc_27_Remove_Element.java
@@ -4,15 +4,12 @@ import java.util.*;
 public class lc_27_Remove_Element {
     public static int removeElement(int[] nums, int val) {
         int i = 0;
-        for (int j = 0; j < nums.length; j++){
-            if (nums[j] != val ){
-                int tmp = nums[i];
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
                 nums[i++] = nums[j];
-                nums[j] = tmp;
-                System.out.println(j);
+
             }
         }
-
         return i;
     }
     public static void main(String[] args){
@@ -23,8 +20,4 @@ public class lc_27_Remove_Element {
         //System.out.println(Arrays.toString(result));
         System.out.println((result));
     }
-
-
-
-
 }
