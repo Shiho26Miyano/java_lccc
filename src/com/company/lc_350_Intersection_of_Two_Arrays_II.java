ad5ff5ad1497ac1a222256afb6116faa1170977a
Elain Hu <besthynlove@gmail.com>
2022-08-15 22:54:32 -0400

    m

--- src/com/company/lc_350_Intersection_of_Two_Arrays_II.java
+++ src/com/company/lc_350_Intersection_of_Two_Arrays_II.java
@@ -2,20 +2,20 @@ package com.company;
 import java.util.*;
 public class lc_350_Intersection_of_Two_Arrays_II {
     public static int[] intersect(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
-            return intersect(nums2, nums1);
-        }
-        HashMap<Integer, Integer> m = new HashMap<>();
-        for (int n : nums1) {
-            m.put(n, m.getOrDefault(n, 0) + 1);
+        if (nums1.length > nums2.length) return intersect(nums2, nums1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums1){
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
         int k = 0;
-        for (int n : nums2) {
-            int cnt = m.getOrDefault(n, 0);
-            if (cnt > 0) {
-                nums1[k++] = n;
-                m.put(n, cnt - 1);
+        for (int num : nums2){
+            int count = map.getOrDefault(num, 0);
+            if (count > 0){
+                nums1[k++] = num;
+                map.put(num, count - 1);
             }
+
         }
         return Arrays.copyOfRange(nums1, 0, k);
         }
