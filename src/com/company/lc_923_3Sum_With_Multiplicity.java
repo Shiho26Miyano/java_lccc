02973b3250b87aeb94c8054dd2ba06fb24861249
Elain Hu <besthynlove@gmail.com>
2022-08-17 08:16:59 -0400

    m

--- src/com/company/lc_923_3Sum_With_Multiplicity.java
+++ src/com/company/lc_923_3Sum_With_Multiplicity.java
@@ -3,51 +3,46 @@ import java.util.*;
 
 public class lc_923_3Sum_With_Multiplicity {
     public static int threeSumMulti(int[] A, int target) {
-        int MOD = 1_000_000_007;
-        long ans = 0;
-        Arrays.sort(A);
-
-        for (int i = 0; i < A.length; ++i) {
-            // We'll try to find the number of i < j < k
-            // with A[j] + A[k] == T, where T = target - A[i].
-
-            // The below is a "two sum with multiplicity".
-            int T = target - A[i];
-            int j = i+1, k = A.length - 1;
-
-            while (j < k) {
-                // These steps proceed as in a typical two-sum.
-                if (A[j] + A[k] < T)
-                    j++;
-                else if (A[j] + A[k] > T)
-                    k--;
-                else if (A[j] != A[k]) {  // We have A[j] + A[k] == T.
-                    // Let's count "left": the number of A[j] == A[j+1] == A[j+2] == ...
-                    // And similarly for "right".
-                    int left = 1, right = 1;
-                    while (j+1 < k && A[j] == A[j+1]) {
-                        left++;
-                        j++;
+        int MOD = 1000000007;
+        Map<Integer, Long> map = new HashMap<>();
+        for (int num : A) {
+            map.put(num, map.getOrDefault(num, 0L) + 1);
+        }
+        long result = 0;
+        for (int num1 : map.keySet()) {
+            for (int num2 : map.keySet()) {
+                int num3 = target - num1 - num2;
+                if (map.containsKey(num3)) {
+                    Long count1 = map.get(num1);
+                    Long count2 = map.get(num2);
+                    Long count3 = map.get(num3);
+                    if (num1 == num2 && num1 == num3) {
+                        result = (result + (count1 * (count1 - 1) * (count1 - 2) / 6)) % MOD;
+                    } else if (num1 == num2 && num1 != num3) {
+                        result = (result + (count1 * (count1 - 1) / 2) * count3) % MOD;
+                    } else if (num1 < num2 && num2 < num3) {
+                        result = (result + count1 * count2 * count3) % MOD;
                     }
-                    while (k-1 > j && A[k] == A[k-1]) {
-                        right++;
-                        k--;
-                    }
-
-                    ans += left * right;
-                    ans %= MOD;
-                    j++;
-                    k--;
-                } else {
-                    // M = k - j + 1
-                    // We contributed M * (M-1) / 2 pairs.
-                    ans += (k-j+1) * (k-j) / 2;
-                    ans %= MOD;
-                    break;
                 }
             }
         }
+        return (int) result;
+    }
+    public static void main(String[] args){
+        int[] nums1 = new int[]{1,1,2,2,3,3,4,4,5,5};
+        //int[] nums2 = new int[]{2,2,3};
+        int val = 8;
+        int result = threeSumMulti(nums1, val);
+        //System.out.println(Arrays.toString(result));
+        System.out.println((result));
+    }
+    public static void print2D(int mat[][])
+    {
+        // Loop through all rows
+        for (int[] row : mat)
 
-        return (int) ans;
+            // converting each row as string
+            // and then printing in a separate line
+            System.out.println(Arrays.toString(row));
     }
 }
