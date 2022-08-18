a50a69d0e8f964300cb85ed40c144dfc16781f24
Elain Hu <besthynlove@gmail.com>
2022-08-17 23:47:50 -0400

    m

---
+++ src/com/company/lc_1696_Jump_Game_V.java
@@ -0,0 +1,47 @@
+package com.company;
+import java.util.*;
+
+
+public class lc_1696_Jump_Game_V {
+    public static int maxResult(int[] nums, int k) {
+        int n = nums.length;
+        int[] score = new int[n];
+        score[0] = nums[0];
+        Deque<Integer> dq = new LinkedList<>();
+        dq.offerLast(0);
+        for (int i = 1; i < n; i++) {
+            // pop the old index
+            while (dq.peekFirst() != null && dq.peekFirst() < i - k) {
+                dq.pollFirst();
+            }
+            score[i] = score[dq.peek()] + nums[i];
+            // pop the smaller value
+            while (dq.peekLast() != null && score[i] >= score[dq.peekLast()]) {
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+        }
+        return score[n - 1];
+    }
+    public static void main(String[] args){
+        int[] nums1 = new int[]{1,-1,-2,4,-7,3};
+        //int[] nums2 = new int[]{2,2,3};
+        int val = 2;
+        int ladders = 1;
+        int result =  maxResult(nums1, val);
+        //System.out.println(Arrays.toString(result));
+        System.out.println((result));
+    }
+
+
+
+    public static void print2D(int mat[][])
+    {
+        // Loop through all rows
+        for (int[] row : mat)
+
+            // converting each row as string
+            // and then printing in a separate line
+            System.out.println(Arrays.toString(row));
+    }
+}
