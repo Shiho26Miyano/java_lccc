efcedb0a524be4294e0ecd2836ecb3b94f46f317
Elain Hu <besthynlove@gmail.com>
2022-08-21 17:25:34 -0400

    m

---
+++ src/com/company/lc_456_132_Pattern.java
@@ -0,0 +1,65 @@
+package com.company;
+import java.util.*;
+
+
+public class lc_456_132_Pattern {
+    public static boolean find132pattern(int[] nums) {
+        if (nums.length < 3)
+            return false;
+        int[] min = new int[nums.length];
+        min[0] = nums[0];
+        for (int i = 1; i < nums.length; i++)
+            min[i] = Math.min(min[i - 1], nums[i]);
+        for (int j = nums.length - 1, k = nums.length; j >= 0; j--) {
+            if (nums[j] > min[j]) {
+                while (k < nums.length && nums[k] <= min[j])
+                    k++;
+                if (k < nums.length && nums[k] < nums[j])
+                    return true;
+                nums[--k] = nums[j];
+            }
+        }
+        return false;
+    }
+    public static boolean find132pattern2(int[] nums) {
+        Deque<Integer> stack = new LinkedList<>();
+
+        int s3 = Integer.MIN_VALUE;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] < s3) {
+                return true;
+            } else {
+                while (!stack.isEmpty() && nums[i] > stack.peek()) {
+                    s3 = Math.max(s3, stack.pop());
+                }
+            }
+            stack.push(nums[i]);
+        }
+
+        return false;
+    }
+
+
+
+    public static void main(String[] args){
+        int[] nums1 = new int[]{0,1,2,4,5,7};
+        //int[] nums2 = new int[]{2,2,3};
+        int val = 2;
+        int ladders = 1;
+        boolean result = find132pattern(nums1);
+        //System.out.println(Arrays.toString(result));
+        System.out.println((result));
+    }
+
+
+
+    public static void print2D(int mat[][])
+    {
+        // Loop through all rows
+        for (int[] row : mat)
+
+            // converting each row as string
+            // and then printing in a separate line
+            System.out.println(Arrays.toString(row));
+    }
+}
