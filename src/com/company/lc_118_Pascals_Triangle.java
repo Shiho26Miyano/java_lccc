9e5712e3906dd165b68da2f995e09e51d376f83d
Elain Hu <besthynlove@gmail.com>
2022-08-22 15:42:28 -0400

    m

--- src/com/company/lc_118_Pascals_Triangle.java
+++ src/com/company/lc_118_Pascals_Triangle.java
@@ -2,33 +2,26 @@ package com.company;
 import java.util.*;
 public class lc_118_Pascals_Triangle {
     public static List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
 
-        // Base case; first row is always [1].
-        triangle.add(new ArrayList<>());
-        triangle.get(0).add(1);
+        result.add(new ArrayList<>());
+        result.get(0).add(1);
 
         for (int rowNum = 1; rowNum < numRows; rowNum++) {
             List<Integer> row = new ArrayList<>();
-            List<Integer> prevRow = triangle.get(rowNum-1);
+            List<Integer> prevRow = result.get(rowNum - 1);
 
-            // The first row element is always 1.
             row.add(1);
 
-            // Each triangle element (other than the first and last of each row)
-            // is equal to the sum of the elements above-and-to-the-left and
-            // above-and-to-the-right.
             for (int j = 1; j < rowNum; j++) {
-                row.add(prevRow.get(j-1) + prevRow.get(j));
+                row.add(prevRow.get(j - 1) + prevRow.get(j));
             }
 
-            // The last row element is always 1.
             row.add(1);
-
-            triangle.add(row);
+            result.add(row);
         }
 
-        return triangle;
+        return result;
     }
 
     public static void main(String[] args){
@@ -52,6 +45,5 @@ public class lc_118_Pascals_Triangle {
             // and then printing in a separate line
             System.out.println(Arrays.toString(row));
     }
-
 
 }
