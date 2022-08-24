6e567480987e4a053b61b19827aeda0db73cf5a6
Elain Hu <besthynlove@gmail.com>
2022-08-23 23:37:30 -0400

    m

---
+++ src/com/company/lc_1029_Two_City_Scheduling.java
@@ -0,0 +1,45 @@
+package com.company;
+import java.util.*;
+
+
+public class lc_1029_Two_City_Scheduling {
+    public static int twoCitySchedCost(int[][] costs) {
+        Arrays.sort(costs, (a, b) -> {
+            return a[0] - a[1] - (b[0] - b[1]);
+        });
+
+        int sum = 0;
+        for (int i = 0; i < costs.length; ++i) {
+            if (i < costs.length / 2) {
+                sum += costs[i][0];
+            } else {
+                sum += costs[i][1];
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args){
+        int[][] matrix = new int[][]{{10,20}, {30,200}, {400, 50}, {30, 20}};
+        //int[] nums2 = new int[]{2,2,3};
+        int val = 2;
+        int ladders = 1;
+        String stringA = "3[a]2[bc]";
+        int result = twoCitySchedCost(matrix);
+        //System.out.println(Arrays.toString(result));
+        System.out.println((result));
+    }
+
+
+
+    public static void print2D(int mat[][])
+    {
+        // Loop through all rows
+        for (int[] row : mat)
+
+            // converting each row as string
+            // and then printing in a separate line
+            System.out.println(Arrays.toString(row));
+    }
+
+}
