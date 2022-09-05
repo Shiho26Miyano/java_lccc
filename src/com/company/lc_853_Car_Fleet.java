1ee97c05e5f69fdb29d549e359612d1c4d0adbe6
Elain Hu <besthynlove@gmail.com>
2022-09-05 18:22:55 -0400

    m

--- src/com/company/lc_853_Car_Fleet.java
+++ src/com/company/lc_853_Car_Fleet.java
@@ -4,25 +4,29 @@ import java.util.*;
 public class lc_853_Car_Fleet {
     public static int carFleet(int target, int[] position, int[] speed) {
         int n = position.length;
-        int[] dist = new int[n];
-        double[] timeAr = new double[n];
+        int[] leftDistance = new int[n];
+        double[] leftHour = new double[n];
         HashMap<Integer, Double> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            dist[i] = target - position[i]; //calculate distance left for each car
-            timeAr[i] = (double) dist[i] / (double) speed[i]; //calculate original time needed for each car
-            map.put(dist[i], timeAr[i]); //map distance left with original time needed
+
+        for (int i = 0; i < n; i++){
+            leftDistance[i] = target - position[i];
+            leftHour[i] = (double) leftDistance[i] / (double) speed[i];
+            map.put(leftDistance[i], leftHour[i]);
         }
 
-        Arrays.sort(dist); //the cars with larger distance left & smaller time needed will chase up the car before
-        List<Double> rTimeL = new ArrayList<Double>(); //store time used for each fleet in ascending order
-        for (int i = 0; i < n; i++) {
-            Double time = map.get(dist[i]);
-            int m = rTimeL.size();
-            if (m == 0 || time > rTimeL.get(m - 1)) {
-                rTimeL.add(time); //new fleet is created
-            }
+        Arrays.sort(leftDistance);
+        List<Double> ansHour = new ArrayList<>();
+
+        for (int i = 0; i < n; i++){
+            Double currentCarHour = map.get(leftDistance[i]);
+            int preCarFleetHour = ansHour.size();
+
+
+            if (preCarFleetHour == 0 || currentCarHour > ansHour.get(preCarFleetHour - 1))
+                ansHour.add(currentCarHour);
+
         }
-        return rTimeL.size();
+        return ansHour.size();
     }
 
     public static void main(String[] args){
