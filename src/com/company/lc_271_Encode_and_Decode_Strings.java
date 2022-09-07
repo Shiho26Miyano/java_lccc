9d4f7404e3915055a36b47bb6867178118c256e9
Elain Hu <besthynlove@gmail.com>
2022-09-06 23:42:11 -0400

    m

---
+++ src/com/company/lc_271_Encode_and_Decode_Strings.java
@@ -0,0 +1,40 @@
+package com.company;
+import java.util.*;
+
+
+public class lc_271_Encode_and_Decode_Strings {
+    // Encodes a list of strings to a single string.
+    public static String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(String s : strs) {
+            sb.append(s.length()).append('/').append(s);
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> ret = new ArrayList<String>();
+        int i = 0;
+        while(i < s.length()) {
+            int slash = s.indexOf('/', i);
+            int size = Integer.valueOf(s.substring(i, slash));
+            i = slash + size + 1;
+            ret.add(s.substring(slash + 1, i));
+        }
+        return ret;
+    }
+    public static void main(String[] args){
+        String[] stringList = new String[]{"Hello","World"};
+        List<String> messages = Arrays.asList("Hello", "World");
+        int[] nums2 = new int[]{2,4,1,1,3};
+        String text = "abbccaa";
+        int val = 12;
+        int ladders = 1;
+
+        System.out.println(messages);
+        //String result =  encode(stringList);
+        //System.out.println(Arrays.toString(result));
+        //System.out.println(result);
+    }
+}
