4e1bb7e9c9a092a1aef031c2c0ff46e399b543af
Elain Hu <besthynlove@gmail.com>
2022-09-06 22:52:36 -0400

    m

--- src/com/company/lc_146_LRU_Cache.java
+++ src/com/company/lc_146_LRU_Cache.java
@@ -2,36 +2,24 @@ package com.company;
 import java.util.*;
 
 public class lc_146_LRU_Cache {
-    private Map<Integer, Integer> map;
-    private Integer capacity;
-
+    HashMap<Integer,Integer> cache;
     public void LRUCache(int capacity) {
-        map = new LinkedHashMap<Integer, Integer>(capacity) {
-            @Override
-            protected boolean removeEldestEntry(Map.Entry eldest) {
+        cache = new LinkedHashMap<>(capacity, 0.75f, true){
+            protected boolean removeEldestEntry(Map.Entry eldest)
+            {
                 return size() > capacity;
             }
         };
-        this.capacity = capacity;
     }
 
     public int get(int key) {
-        if (map.containsKey(key)) {
-            Integer i = map.get(key);
-            map.remove(key);
-            map.put(key, i);
-            return i;
+        if(cache.containsKey(key)){
+            return cache.get(key);
         }
         return -1;
     }
 
     public void put(int key, int value) {
-        if (value < 0) return;
-
-        if (map.containsKey(key)) {
-            map.remove(key);
-        }
-
-        map.put(key, value);
+        cache.put(key, value);
     }
 }
