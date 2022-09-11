a6ccfe27c8daf310667e493f7d2dc266afc0bc84
Elain Hu <besthynlove@gmail.com>
2022-09-10 22:50:27 -0400

    m

--- src/com/company/lc_36_Valid_Sudoku.java
+++ src/com/company/lc_36_Valid_Sudoku.java
@@ -2,22 +2,46 @@ package com.company;
 import java.util.*;
 
 public class lc_36_Valid_Sudoku {
-    public boolean isValidSudoku(char[][] board) {
+    public static boolean isValidSudoku(char[][] board) {
+        int N = 9;
 
-        boolean[][] row = new boolean[9][9];
-        boolean[][] column = new boolean[9][9];
-        boolean[][] block = new boolean[9][9];
+        // Use hash set to record the status
+        HashSet<Character>[] rows = new HashSet[N];
+        HashSet<Character>[] cols = new HashSet[N];
+        HashSet<Character>[] boxes = new HashSet[N];
+        for (int r = 0; r < N; r++) {
+            rows[r] = new HashSet<Character>();
+            cols[r] = new HashSet<Character>();
+            boxes[r] = new HashSet<Character>();
+        }
 
-        for(int i = 0;i<9;i++){
-            for(int j=0;j<9;j++){
-                int c = board[i][j] - '1';
-                if(board[i][j]=='.'){
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                char val = board[r][c];
+
+                // Check if the position is filled with number
+                if (val == '.') {
                     continue;
                 }
-                if(row[i][c]||column[j][c]||block[i - i % 3 + j / 3][c]){
+
+                // Check the row
+                if (rows[r].contains(val)) {
                     return false;
                 }
-                row[i][c] = column[j][c] = block[i - i % 3 + j / 3][c] = true;
+                rows[r].add(val);
+
+                // Check the column
+                if (cols[c].contains(val)) {
+                    return false;
+                }
+                cols[c].add(val);
+
+                // Check the box
+                int idx = (r / 3) * 3 + c / 3;
+                if (boxes[idx].contains(val)) {
+                    return false;
+                }
+                boxes[idx].add(val);
             }
         }
         return true;
