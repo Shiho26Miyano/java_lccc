e50c0dff837556d092ac4c76b2802d4759d19e2e
Elain Hu <besthynlove@gmail.com>
2022-09-10 23:11:07 -0400

    m

--- src/com/company/lc_36_Valid_Sudoku.java
+++ src/com/company/lc_36_Valid_Sudoku.java
@@ -2,46 +2,40 @@ package com.company;
 import java.util.*;
 
 public class lc_36_Valid_Sudoku {
-    public static boolean isValidSudoku(char[][] board) {
+    public boolean isValidSudoku(char[][] board) {
         int N = 9;
 
-        // Use hash set to record the status
-        HashSet<Character>[] rows = new HashSet[N];
-        HashSet<Character>[] cols = new HashSet[N];
-        HashSet<Character>[] boxes = new HashSet[N];
-        for (int r = 0; r < N; r++) {
-            rows[r] = new HashSet<Character>();
-            cols[r] = new HashSet<Character>();
-            boxes[r] = new HashSet<Character>();
-        }
+        // Use an array to record the status
+        int[][] rows = new int[N][N];
+        int[][] cols = new int[N][N];
+        int[][] boxes = new int[N][N];
 
         for (int r = 0; r < N; r++) {
             for (int c = 0; c < N; c++) {
-                char val = board[r][c];
-
                 // Check if the position is filled with number
-                if (val == '.') {
+                if (board[r][c] == '.') {
                     continue;
                 }
+                int pos = board[r][c] - '1';
 
                 // Check the row
-                if (rows[r].contains(val)) {
+                if (rows[r][pos] == 1) {
                     return false;
                 }
-                rows[r].add(val);
+                rows[r][pos] = 1;
 
                 // Check the column
-                if (cols[c].contains(val)) {
+                if (cols[c][pos] == 1) {
                     return false;
                 }
-                cols[c].add(val);
+                cols[c][pos] = 1;
 
                 // Check the box
                 int idx = (r / 3) * 3 + c / 3;
-                if (boxes[idx].contains(val)) {
+                if (boxes[idx][pos] == 1) {
                     return false;
                 }
-                boxes[idx].add(val);
+                boxes[idx][pos] = 1;
             }
         }
         return true;
