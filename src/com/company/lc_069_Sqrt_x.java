ebc6c8d0a06da662787d9295f84d70531cb044e6
Elain Hu <besthynlove@gmail.com>
2022-09-12 23:59:29 -0400

    m

--- src/com/company/lc_069_Sqrt_x.java
+++ src/com/company/lc_069_Sqrt_x.java
@@ -5,24 +5,29 @@ import java.util.Arrays;
 public class lc_069_Sqrt_x {
 
     public static int mySqrt(int x) {
-        if (x < 2) return x;
 
-        long num;
-        int pivot, left = 2, right = x / 2;
-        while (left <= right) {
+        if(x==0 || x==1)
+            return x;
 
-            System.out.println(left);
-            System.out.println(right);
-            pivot = left + (right - left) / 2;
-            System.out.println(pivot);
-            num = (long)pivot * pivot;
-            if (num > x) right = pivot - 1;
-            else if (num < x) left = pivot + 1;
-            else return pivot;
+        int start=1;
+        int end=x/2;
+        int ans=0;
+
+
+
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+
+            if(mid<=x/mid)          //To escape the INT OVERFLOW
+            {ans=mid;
+                start=mid+1;}
+            else
+                end=mid-1;
         }
-        return right;
-    }
+        return ans;
 
+    }
 
 
     public static void main(String[] args){
