94013d04a458548a4c51911151d5fd51f5af329b
Elain Hu <besthynlove@gmail.com>
2022-09-12 23:21:39 -0400

    m

--- src/com/company/lc_424_Longest_Repeating_Character_Replacement.java
+++ src/com/company/lc_424_Longest_Repeating_Character_Replacement.java
@@ -2,18 +2,24 @@ package com.company;
 import java.util.*;
 public class lc_424_Longest_Repeating_Character_Replacement {
     public static int characterReplacement(String s, int k) {
-        int len = s.length();
-        int[] count = new int[26];
-        int start = 0, maxCount = 0, maxLength = 0;
-        for (int end = 0; end < len; end++) {
-            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
-            while (end - start + 1 - maxCount > k) {
-                count[s.charAt(start) - 'A']--;
-                start++;
+        int[] cArr = new int[26];
+        int maxCount = 0, start = 0, maxSize = 0;
+
+        for(int end = 0; end < s.length(); end ++){
+            cArr[s.charAt(end) - 'A']++;
+            maxCount = Math.max(maxCount, cArr[s.charAt(end) - 'A']);
+
+            // if max character frequency + distance between start and end is greater than k
+            // this means we have considered changing more than k charactres. So time to shrink window
+            if(end - start + 1 - maxCount > k){
+                cArr[s.charAt(start) - 'A']--;
+                start ++;
             }
-            maxLength = Math.max(maxLength, end - start + 1);
+
+            maxSize = Math.max(maxSize, end - start + 1);
         }
-        return maxLength;
+
+        return maxSize;
     }
     public static void main(String[] args) {
         int[] intArray = new int[]{1, 3, 5, 6, 7};
