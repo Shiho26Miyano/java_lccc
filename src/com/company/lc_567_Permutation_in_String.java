a3ca47abe61c7787b794b5d8c8c6518a1f9c1a34
Elain Hu <besthynlove@gmail.com>
2022-09-13 23:40:21 -0400

    m

--- src/com/company/lc_567_Permutation_in_String.java
+++ src/com/company/lc_567_Permutation_in_String.java
@@ -2,24 +2,34 @@ package com.company;
 import java.util.*;
 
 public class lc_567_Permutation_in_String {
-    public int characterReplacement(String s, int k) {
-        int[] cArr = new int[26];
-        int maxCount = 0, start = 0, maxSize = 0;
-
-        for(int end = 0; end < s.length(); end ++){
-            cArr[s.charAt(end) - 'A']++;
-            maxCount = Math.max(maxCount, cArr[s.charAt(end) - 'A']);
-
-            // if max character frequency + distance between start and end is greater than k
-            // this means we have considered changing more than k charactres. So time to shrink window
-            if(end - start + 1 - maxCount > k){
-                cArr[s.charAt(start) - 'A']--;
-                start ++;
-            }
-
-            maxSize = Math.max(maxSize, end - start + 1);
+    public boolean checkInclusion(String s1, String s2) {
+        //The goal is count the occurrences of each character.
+        // c - 'a'
+        //is a kind of clever way to get the position of the character in the alphabet. 'a' - 'a' would give you 0.
+        //'b' - 'a' would give you 1. 'c' - 'a' would give you 2, and so on.
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1map = new int[26];
+        int[] s2map = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1map[s1.charAt(i) - 'a']++;
+            System.out.println(Arrays.toString(s1map));
+            s2map[s2.charAt(i) - 'a']++;
         }
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            if (matches(s1map, s2map))
+                return true;
+            s2map[s2.charAt(i + s1.length()) - 'a']++;
+            s2map[s2.charAt(i) - 'a']--;
+        }
+        return matches(s1map, s2map);
+    }
 
-        return maxSize;
+    public boolean matches(int[] s1map, int[] s2map) {
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] != s2map[i])
+                return false;
+        }
+        return true;
     }
 }
