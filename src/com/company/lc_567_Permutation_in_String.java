1d2bd06447ce8b04ed772a88696ab0b68df588ec
Elain Hu <besthynlove@gmail.com>
2022-09-14 00:46:16 -0400

    m

--- src/com/company/lc_567_Permutation_in_String.java
+++ src/com/company/lc_567_Permutation_in_String.java
@@ -1,28 +1,34 @@
 package com.company;
 import java.util.*;
-
+//The goal is count the occurrences of each character.
+// c - 'a'
+//is a kind of clever way to get the position of the character in the alphabet. 'a' - 'a' would give you 0.
+//'b' - 'a' would give you 1. 'c' - 'a' would give you 2, and so on.
 public class lc_567_Permutation_in_String {
     public boolean checkInclusion(String s1, String s2) {
-        //The goal is count the occurrences of each character.
-        // c - 'a'
-        //is a kind of clever way to get the position of the character in the alphabet. 'a' - 'a' would give you 0.
-        //'b' - 'a' would give you 1. 'c' - 'a' would give you 2, and so on.
-        if (s1.length() > s2.length())
-            return false;
-        int[] s1map = new int[26];
-        int[] s2map = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
-            s1map[s1.charAt(i) - 'a']++;
-            System.out.println(Arrays.toString(s1map));
-            s2map[s2.charAt(i) - 'a']++;
+        if(s1.length() > s2.length()) return false;
+
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+
+        for(int i = 0; i < s1.length(); i++){
+            arr1[s1.charAt(i) - 'a']++;
+            arr2[s2.charAt(i) - 'a']++;
         }
-        for (int i = 0; i < s2.length() - s1.length(); i++) {
-            if (matches(s1map, s2map))
-                return true;
-            s2map[s2.charAt(i + s1.length()) - 'a']++;
-            s2map[s2.charAt(i) - 'a']--;
+
+        if(Arrays.equals(arr1, arr2)) return true;
+
+        int front = 0;
+        int back = s1.length();
+        while(back < s2.length()){
+            arr2[s2.charAt(front) - 'a']--;
+            arr2[s2.charAt(back) - 'a']++;
+
+            if(Arrays.equals(arr1, arr2)) return true;
+            front++;
+            back++;
         }
-        return matches(s1map, s2map);
+        return false;
     }
 
     public boolean matches(int[] s1map, int[] s2map) {
