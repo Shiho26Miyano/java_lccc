fb541f0b930c2eb51138b68f0dc346db623fddd6
Elain Hu <besthynlove@gmail.com>
2022-09-17 12:52:23 -0400

    m

---
+++ src/com/company/lc_340_Longest_Substring_with_At_Most_K_Distinct_Characters.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class lc_340_Longest_Substring_with_At_Most_K_Distinct_Characters {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int[] count = new int[256];
+        int num = 0, i = 0, res = 0;
+        for (int j = 0; j < s.length(); j++) {
+            if (count[s.charAt(j)]++ == 0) num++;
+            if (num > k) {
+                while (--count[s.charAt(i++)] > 0);
+                num--;
+            }
+            res = Math.max(res, j - i + 1);
+        }
+        return res;
+    }
+}
