920945bd00b64d78ce61fa814809829107783cfd
Elain Hu <besthynlove@gmail.com>
2022-09-18 22:30:49 -0400

    m

--- src/com/company/lc_424_Longest_Repeating_Character_Replacement.java
+++ src/com/company/lc_424_Longest_Repeating_Character_Replacement.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class lc_424_Longest_Repeating_Character_Replacement {
     public static int characterReplacement(String s, int k) {
         int len = s.length();
-        int[] count = new int[26];
+        int[] map = new int[26];
         int start = 0, maxCount = 0, maxLength = 0;
         for (int end = 0; end < len; end++) {
-            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
+            maxCount = Math.max(maxCount, ++map[s.charAt(end) - 'A']);
             while (end - start + 1 - maxCount > k) {
-                count[s.charAt(start) - 'A']--;
+                map[s.charAt(start) - 'A']--;
                 start++;
             }
             maxLength = Math.max(maxLength, end - start + 1);
