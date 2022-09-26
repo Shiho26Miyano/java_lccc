9704c30b7114777342479df9fb3f6c3e3f6c934c
Elain Hu <besthynlove@gmail.com>
2022-09-25 23:25:47 -0400

    m

--- src/com/company/lc_1248_Count_Number_of_Nice_Subarrays.java
+++ src/com/company/lc_1248_Count_Number_of_Nice_Subarrays.java
@@ -7,13 +7,16 @@ public class lc_1248_Count_Number_of_Nice_Subarrays {
     }
 
     public int atMost(int[] A, int k) {
-        int res = 0, i = 0, n = A.length;
-        for (int j = 0; j < n; j++) {
-            k -= A[j] % 2;
-            while (k < 0)
-                k += A[i++] % 2;
-            res += j - i + 1;
+        int best = 0, l = 0;
+        for (int r = 0; r < A.length; r++){
+            k = k - A[r] % 2;
+            while (k < 0){
+                k = k + A[l]% 2;
+                l++;
+            }
+            best = best + r - l + 1;
         }
-        return res;
+        return best;
+
     }
 }
