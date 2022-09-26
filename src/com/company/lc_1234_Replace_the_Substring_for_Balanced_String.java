44d5c11d537455a7d60fc2611801306c3b1f8a25
Elain Hu <besthynlove@gmail.com>
2022-09-25 23:16:18 -0400

    m

---
+++ src/com/company/lc_1234_Replace_the_Substring_for_Balanced_String.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.*;
+
+
+public class lc_1234_Replace_the_Substring_for_Balanced_String {
+    public int balancedString(String s) {
+        int[] count = new int[128];
+        int n = s.length(), res = n, i = 0, k = n / 4;
+        for (int j = 0; j < n; ++j) {
+            ++count[s.charAt(j)];
+        }
+        for (int j = 0; j < n; ++j) {
+            --count[s.charAt(j)];
+            while (i < n && count['Q'] <= k && count['W'] <= k && count['E'] <= k && count['R'] <= k) {
+                res = Math.min(res, j - i + 1);
+                ++count[s.charAt(i++)];
+            }
+        }
+        return res;
+    }
+}
