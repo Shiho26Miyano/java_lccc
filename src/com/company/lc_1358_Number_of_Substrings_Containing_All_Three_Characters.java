8af4264e6f3c4b7b4dabaead13319fc40efb4fc1
Elain Hu <besthynlove@gmail.com>
2022-09-26 00:18:59 -0400

    m

--- src/com/company/lc_1358_Number_of_Substrings_Containing_All_Three_Characters.java
+++ src/com/company/lc_1358_Number_of_Substrings_Containing_All_Three_Characters.java
@@ -4,13 +4,13 @@ import java.util.*;
 
 public class lc_1358_Number_of_Substrings_Containing_All_Three_Characters {
     public int numberOfSubstrings(String s) {
-        int count[] = {0, 0, 0}, res = 0 , i = 0, n = s.length();
-        for (int j = 0; j < n; ++j) {
-            ++count[s.charAt(j) - 'a'];
+        int count[] = {0, 0, 0}, best = 0 , l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            count[s.charAt(r) - 'a']++;
             while (count[0] > 0 && count[1] > 0 && count[2] > 0)
-                --count[s.charAt(i++) - 'a'];
-            res += i;
+                count[s.charAt(l++) - 'a']--;
+            best += l;
         }
-        return res;
+        return best;
     }
 }
