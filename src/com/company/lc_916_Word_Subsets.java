d59fff6ad07220f01987abdcec8f989f0cdce8b7
Elain Hu <besthynlove@gmail.com>
2022-10-07 17:11:01 -0400

    m

--- src/com/company/lc_916_Word_Subsets.java
+++ src/com/company/lc_916_Word_Subsets.java
@@ -2,26 +2,30 @@ package com.company;
 import java.util.*;
 
 public class lc_916_Word_Subsets {
-    public List<String> wordSubsets(String[] A, String[] B) {
+    public List<String> wordSubsets(String[] words1, String[] words2) {
         int[] count = new int[26], tmp;
         int i;
-        for (String b : B) {
-            tmp = counter(b);
-            for (i = 0; i < 26; ++i)
+        for (String word: words2){
+            tmp = counter(word);
+            for (i = 0; i < 26; i++){
                 count[i] = Math.max(count[i], tmp[i]);
+            }
         }
         List<String> res = new ArrayList<>();
-        for (String a : A) {
-            tmp = counter(a);
-            for (i = 0; i < 26; ++i)
+        for (String word: words1){
+            tmp = counter(word);
+            for(i = 0; i < 26; i++){
                 if (tmp[i] < count[i])
                     break;
-            if (i == 26) res.add(a);
+            }
+            if(i == 26) res.add(word);
+            
         }
         return res;
+
     }
 
-    int[] counter(String word) {
+    int[] counter(String word){
         int[] count = new int[26];
         for (char c : word.toCharArray()) count[c - 'a']++;
         return count;
