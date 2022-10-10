589fa3e3a1260418479b150f14ca1781880a93bb
Elain Hu <besthynlove@gmail.com>
2022-10-10 16:20:44 -0400

    m

--- src/com/company/lc_916_Word_Subsets.java
+++ src/com/company/lc_916_Word_Subsets.java
@@ -6,31 +6,35 @@ public class lc_916_Word_Subsets {
     //we use function counter to count occurrence of each letter.
     //We take the maximum occurrences of counts, and use it as a filter of A.
     public List<String> wordSubsets(String[] words1, String[] words2) {
-        int[] count = new int[26], tmp;
-        int i;
+        int[] countMap = new int[26], tmp;
+        int i ;
+
         for (String word: words2){
             tmp = counter(word);
             // 创建mapping list把所有word打散算出所有的frequency
             for (i = 0; i < 26; i++){
-                count[i] = Math.max(count[i], tmp[i]);
+                countMap[i] = Math.max(countMap[i], tmp[i]);
             }
         }
-        List<String> res = new ArrayList<>();
+        List<String> ans = new ArrayList<>();
+
         for (String word: words1){
             tmp = counter(word);
             //filter每一个词character的frequency是否大于等于mapping list；如果小于那就不要了
-            for(i = 0; i < 26; i++){
-                if (tmp[i] < count[i])
-                    break;
+            for (i = 0; i < 26; i++){
+                if (tmp[i] < countMap[i]) break;
             }
-            if(i > 25) res.add(word);
+            if (i > 25) ans.add(word);
         }
-        return res;
+        return ans;
+
     }
+
 
     int[] counter(String word){
         int[] count = new int[26];
         for (char c : word.toCharArray()) count[c - 'a']++;
         return count;
+
     }
 }
