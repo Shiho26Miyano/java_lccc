97296c1199343feb4175fc8f5b12c2e0025fbd02
Elain Hu <besthynlove@gmail.com>
2022-10-11 12:46:26 -0400

    m

--- src/com/company/lc_937_Reorder_Data_in_Log_Files.java
+++ src/com/company/lc_937_Reorder_Data_in_Log_Files.java
@@ -3,32 +3,31 @@ import java.util.*;
 
 public class lc_937_Reorder_Data_in_Log_Files {
     public String[] reorderLogFiles(String[] logs) {
-
-        Comparator<String> myComp = new Comparator<String>() {
+        Comparator<String> customComp = new Comparator<String>(){
             //Arrays.sort(logs, (a,b)->{
-            @Override
-            public int compare(String a, String b) {
-                // split each log into two parts: <identifier, content>
+            public int compare(String a, String b){
                 int identA = a.indexOf(" ") + 1;
                 int identB = b.indexOf(" ") + 1;
 
                 boolean isLetterA = Character.isLetter(a.charAt(identA));
                 boolean isLetterB = Character.isLetter(b.charAt(identB));
+
                 if(isLetterA && isLetterB){
+                    // first compare the content
                     int cmp = a.substring(identA).compareTo(b.substring(identB));
                     if(cmp != 0) return cmp;
-
+                    // logs of same content, compare the identifiers
                     return a.compareTo(b);
-                }else if(isLetterA && !isLetterB){
+                }else if (isLetterA && !isLetterB){
                     return -1;
                 }else if(!isLetterA && isLetterB){
                     return 1;
                 }else return 0;
-
             }
-        };
 
-        Arrays.sort(logs, myComp);
+        };
+        Arrays.sort(logs, customComp);
         return logs;
+
     }
 }
