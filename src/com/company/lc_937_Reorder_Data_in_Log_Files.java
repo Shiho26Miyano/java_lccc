b1dd773f4a58ddab2672a1321f566486a8a0bee4
Elain Hu <besthynlove@gmail.com>
2022-10-11 11:59:36 -0400

    m

--- src/com/company/lc_937_Reorder_Data_in_Log_Files.java
+++ src/com/company/lc_937_Reorder_Data_in_Log_Files.java
@@ -4,25 +4,31 @@ import java.util.*;
 public class lc_937_Reorder_Data_in_Log_Files {
     public String[] reorderLogFiles(String[] logs) {
 
-        Arrays.sort(logs, (a,b)->{
-            int identA = a.indexOf(" ") + 1;
-            int identB = b.indexOf(" ") + 1;
+        Comparator<String> myComp = new Comparator<String>() {
+            //Arrays.sort(logs, (a,b)->{
+            @Override
+            public int compare(String a, String b) {
+                // split each log into two parts: <identifier, content>
+                int identA = a.indexOf(" ") + 1;
+                int identB = b.indexOf(" ") + 1;
 
-            boolean isLetterA = Character.isLetter(a.charAt(identA));
-            boolean isLetterB = Character.isLetter(b.charAt(identB));
-            if(isLetterA && isLetterB){
-                int cmp = a.substring(identA).compareTo(b.substring(identB));
-                if(cmp != 0) return cmp;
+                boolean isLetterA = Character.isLetter(a.charAt(identA));
+                boolean isLetterB = Character.isLetter(b.charAt(identB));
+                if(isLetterA && isLetterB){
+                    int cmp = a.substring(identA).compareTo(b.substring(identB));
+                    if(cmp != 0) return cmp;
 
-                return a.compareTo(b);
-            }else if(isLetterA && !isLetterB){
-                return -1;
-            }else if(!isLetterA && isLetterB){
-                return 1;
-            }else return 0;
+                    return a.compareTo(b);
+                }else if(isLetterA && !isLetterB){
+                    return -1;
+                }else if(!isLetterA && isLetterB){
+                    return 1;
+                }else return 0;
 
-        });
+            }
+        };
 
+        Arrays.sort(logs, myComp);
         return logs;
     }
 }
