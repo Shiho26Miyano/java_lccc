05e3557753314fcf8784794dfcbdaf1c621b71e9
Elain Hu <besthynlove@gmail.com>
2022-10-10 23:35:27 -0400

    m

--- src/com/company/lc_937_Reorder_Data_in_Log_Files.java
+++ src/com/company/lc_937_Reorder_Data_in_Log_Files.java
@@ -1,30 +1,28 @@
 package com.company;
+import java.util.*;
 
 public class lc_937_Reorder_Data_in_Log_Files {
     public String[] reorderLogFiles(String[] logs) {
-        Arrays.sort(logs, (s1, s2) -> {
-            String[] split1 = s1.split(" ", 2);
-            String[] split2 = s2.split(" ", 2);
 
-            boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
-            boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
+        Arrays.sort(logs, (a,b)->{
+            int identA = a.indexOf(" ") + 1;
+            int identB = b.indexOf(" ") + 1;
 
-            if(!isDigit1 && !isDigit2) {
-                // both letter-logs.
-                int comp = split1[1].compareTo(split2[1]);
-                if (comp == 0) return split1[0].compareTo(split2[0]);
-                else return comp;
-            } else if (isDigit1 && isDigit2) {
-                // both digit-logs. So keep them in original order
-                return 0;
-            } else if (isDigit1 && !isDigit2) {
-                // first is digit, second is letter. bring letter to forward.
-                return 1;
-            } else {
-                //first is letter, second is digit. keep them in this order.
+            boolean isLetterA = Character.isLetter(a.charAt(identA));
+            boolean isLetterB = Character.isLetter(b.charAt(identB));
+            if(isLetterA && isLetterB){
+                int cmp = a.substring(identA).compareTo(b.substring(identB));
+                if(cmp != 0) return cmp;
+
+                return a.compareTo(b);
+            }else if(isLetterA && !isLetterB){
                 return -1;
-            }
+            }else if(!isLetterA && isLetterB){
+                return 1;
+            }else return 0;
+
         });
+
         return logs;
     }
 }
