ff1c6edc67a11b9590305e6d31ec32fd56e6c1b5
Elain Hu <besthynlove@gmail.com>
2022-10-17 22:07:47 -0400

    m

---
+++ src/com/company/lc_205_Isomorphic_Strings.java
@@ -0,0 +1,53 @@
+package com.company;
+import java.util.*;
+
+public class lc_205_Isomorphic_Strings {
+    //1.  use small arrays as lookup tables. it uses the arrays to minic two hash table, but more efficient than hash table.
+    public boolean isIsomorphic(String sString, String tString) {
+
+        char[] s = sString.toCharArray();
+        char[] t = tString.toCharArray();
+
+        int length = s.length;
+        if(length != t.length) return false;
+
+        char[] sm = new char[256];
+        char[] tm = new char[256];
+
+        for(int i=0; i<length; i++){
+            char sc = s[i];
+            char tc = t[i];
+            if(sm[sc] == 0 && tm[tc] == 0){
+                sm[sc] = tc;
+                tm[tc] = sc;
+            }else{
+                if(sm[sc] != tc || tm[tc] != sc){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    //2. hashtable
+    public boolean isIsomorphic2(String s, String t) {
+        if(s == null || s.length() <= 1) return true;
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        for(int i = 0 ; i< s.length(); i++){
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+            if(map.containsKey(a)){
+                if(map.get(a).equals(b))
+                    continue;
+                else
+                    return false;
+            }else{
+                if(!map.containsValue(b))
+                    map.put(a,b);
+                else return false;
+
+            }
+        }
+        return true;
+
+    }
+}
