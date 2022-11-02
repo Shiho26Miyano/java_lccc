246ec48b1c786c10703579781f9bc00cce24ae18
Elain Hu <besthynlove@gmail.com>
2022-11-01 23:02:36 -0400

    m

--- src/com/company/lc_2244_Minimum_Rounds_to_Complete_All_Tasks.java
+++ src/com/company/lc_2244_Minimum_Rounds_to_Complete_All_Tasks.java
@@ -1,15 +1,17 @@
 package com.company;
 import java.util.*;;
 public class lc_2244_Minimum_Rounds_to_Complete_All_Tasks {
-    public int minimumRounds(int[] A) {
+    public int minimumRounds(int[] tasks) {
         HashMap<Integer, Integer> count = new HashMap<>();
-        for (int a : A)
+        for (int a: tasks){
             count.put(a, count.getOrDefault(a, 0) + 1);
-        int res = 0;
-        for (int freq : count.values()) {
+        }
+        int res= 0;
+        for (int freq : count.values()){
             if (freq == 1) return -1;
             res += (freq + 2) / 3;
         }
         return res;
+
     }
 }
