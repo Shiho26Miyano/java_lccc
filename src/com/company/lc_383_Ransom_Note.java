6f8fec8750fb077ff47432a89768df4c2b657222
Elain Hu <besthynlove@gmail.com>
2022-11-06 23:59:24 -0500

    m

---
+++ src/com/company/lc_383_Ransom_Note.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.*;
+
+public class lc_383_Ransom_Note {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabets_counter = new int[26];
+
+        for (char c : magazine.toCharArray())
+            alphabets_counter[c-'a']++;
+
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) return false;
+            alphabets_counter[c-'a']--;
+        }
+        return true;
+    }
+}
