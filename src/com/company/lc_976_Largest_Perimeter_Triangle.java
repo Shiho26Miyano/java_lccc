8162fa5106dc97ee0718304337c9d84c4eda1dfd
Elain Hu <besthynlove@gmail.com>
2022-11-06 23:27:10 -0500

    m

--- src/com/company/lc_976_Largest_Perimeter_Triangle.java
+++ src/com/company/lc_976_Largest_Perimeter_Triangle.java
@@ -2,11 +2,15 @@ package com.company;
 import java.util.*;
 
 public class lc_976_Largest_Perimeter_Triangle {
-    public int largestPerimeter(int[] A) {
-        Arrays.sort(A);
-        for (int i = A.length - 3; i >= 0; --i)
-            if (A[i] + A[i+1] > A[i+2])
-                return A[i] + A[i+1] + A[i+2];
+    public int largestPerimeter(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        for(int i=n-1;i>1;i--){
+            if(nums[i]<nums[i-1]+nums[i-2]){
+                return nums[i]+nums[i-1]+nums[i-2];
+            }
+        }
         return 0;
+
     }
 }
