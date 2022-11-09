fa6b9a0f752d5e5aaf50a0104d09216c064ef5f1
Elain Hu <besthynlove@gmail.com>
2022-11-08 21:32:36 -0500

    m

---
+++ src/com/company/lc_1130_Minimum_Cost_Tree_From_Leaf_Values.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.*;
+
+public class lc_1130_Minimum_Cost_Tree_From_Leaf_Values {
+    public int mctFromLeafValues(int[] A) {
+        int res = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(Integer.MAX_VALUE);
+        for (int a : A) {
+            while (stack.peek() <= a) {
+                int mid = stack.pop();
+                res += mid * Math.min(stack.peek(), a);
+            }
+            stack.push(a);
+        }
+        while (stack.size() > 2) {
+            res += stack.pop() * stack.peek();
+        }
+        return res;
+    }
+}
