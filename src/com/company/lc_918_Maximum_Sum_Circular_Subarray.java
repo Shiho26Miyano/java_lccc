1dd7ad058cb794413b2f442d17797fbe0133ad06
Elain Hu <besthynlove@gmail.com>
2022-11-08 21:28:30 -0500

    m

--- src/com/company/lc_918_Maximum_Sum_Circular_Subarray.java
+++ src/com/company/lc_918_Maximum_Sum_Circular_Subarray.java
@@ -1,15 +1,15 @@
 package com.company;
 import java.util.*;
 public class lc_918_Maximum_Sum_Circular_Subarray {
-    public int maxSubarraySumCircular(int[] A) {
-        int total = 0, maxSum = A[0], curSumMax = 0, minSum = A[0], curSumMin = 0;
-        for (int a : A) {
-            curSumMax = Math.max(curSumMax + a, a);
-            maxSum = Math.max(maxSum, curSumMax);
-            curSumMin = Math.min(curSumMin + a, a);
-            minSum = Math.min(minSum, curSumMin);
-            total += a;
+    public int maxSubarraySumCircular(int[] nums) {
+        int total = 0, maxSum = nums[0], curSumMax = 0, minSum = nums[0], curSumMin = 0;
+        for (int n : nums){
+            curSumMax = Math.max(curSumMax + n, n);
+            maxSum = Math.max(curSumMax, maxSum);
+            curSumMin = Math.min(curSumMin + n, n);
+            minSum = Math.min(curSumMin, minSum);
+            total += n;
         }
-        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
+        return maxSum > 0 ? Math.max(maxSum, total -  minSum) : maxSum;
     }
 }
