13164006b8cc99775bade6962a5727eb16e5cca1
Elain Hu <besthynlove@gmail.com>
2022-11-14 23:21:47 -0500

    m

--- src/com/company/lc_720_Longest_Word_in_Dictionary.java
+++ src/com/company/lc_720_Longest_Word_in_Dictionary.java
@@ -3,11 +3,14 @@ import java.util.*;
 public class lc_720_Longest_Word_in_Dictionary {
     public String longestWord(String[] words) {
         Arrays.sort(words);
+        System.out.println(Arrays.toString(words));
         Set<String> built = new HashSet<String>();
         String res = "";
         for (String w : words) {
+            System.out.println(built);
             if (w.length() == 1 || built.contains(w.substring(0, w.length() - 1))) {
-                res = w.length() > res.length() ? w : res;
+                res = w.length() <= res.length() ? res : w;
+
                 built.add(w);
             }
         }
