c382ae1df652710d49e5b6a3a53b945c06b200d0
Elain Hu <besthynlove@gmail.com>
2022-11-16 00:14:39 -0500

    m

---
+++ src/com/company/lc_303_Range_Sum_Query_Immutable.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class lc_303_Range_Sum_Query_Immutable {
+
+    int[] nums;
+
+    public void NumArray(int[] nums) {
+        for(int i = 1; i < nums.length; i++)
+            nums[i] += nums[i - 1];
+
+        this.nums = nums;
+    }
+
+    public int sumRange(int i, int j) {
+        if(i == 0)
+            return nums[j];
+
+        return nums[j] - nums[i - 1];
+    }
+}
