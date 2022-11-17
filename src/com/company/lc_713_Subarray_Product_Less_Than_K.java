504a87eeee280afd2769c9249bb89a16d4c114f7
Elain Hu <besthynlove@gmail.com>
2022-11-16 23:23:41 -0500

    m

--- src/com/company/lc_713_Subarray_Product_Less_Than_K.java
+++ src/com/company/lc_713_Subarray_Product_Less_Than_K.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class lc_713_Subarray_Product_Less_Than_K {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int n = nums.length,i=0,pro=1,ct=0;
+        int n = nums.length,i = 0,pro = 1,ct = 0;
         for(int j = 0;j < n;j++){
             pro *= nums[j];
             if(pro >= k){
