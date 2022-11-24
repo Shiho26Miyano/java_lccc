dc5f1ee03bd7bdc1384bd2c4135a5fe0fbc4a5e2
Elain Hu <besthynlove@gmail.com>
2022-11-24 10:39:07 -0500

    m

--- src/com/company/lc_303_Range_Sum_Query_Immutable.java
+++ src/com/company/lc_303_Range_Sum_Query_Immutable.java
@@ -2,16 +2,12 @@ package com.company;
 import java.util.*;
 public class lc_303_Range_Sum_Query_Immutable {
     int[] nums;
-
-    public NumArray(int[] nums) {
+    public void NumArray(int[] nums) {
         for (int i = 1; i < nums.length; i++){
             nums[i] += nums[i - 1];
             this.nums = nums;
         }
-
-
     }
-
     public int sumRange(int left, int right) {
         if (left == 0) return nums[right];
         return nums[right] - nums[left - 1];
