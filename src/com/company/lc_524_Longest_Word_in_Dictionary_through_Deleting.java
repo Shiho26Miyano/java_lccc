8c3384c98473a26663879d0e5356471b3a8c352a
Elain Hu <besthynlove@gmail.com>
2022-11-26 12:23:26 -0500

    m

--- src/com/company/lc_524_Longest_Word_in_Dictionary_through_Deleting.java
+++ src/com/company/lc_524_Longest_Word_in_Dictionary_through_Deleting.java
@@ -3,22 +3,22 @@ import java.util.*;
 
 public class lc_524_Longest_Word_in_Dictionary_through_Deleting {
     public String findLongestWord(String s, List<String> dictionary) {
+        // entire dictWord should be found in s
         String longest = "";
-        for (String dictword: dictionary){
+        for (String dictWord : dictionary){
             int i = 0;
             for (char c : s.toCharArray()){
-                if (i < dictword.length() && c == dictword.charAt(i)){
+                if (i < dictWord.length() && c == dictWord.charAt(i)){
                     i++;
                 }
             }
-            if (i == dictword.length() && dictword.length() >= longest.length()){
-                if (dictword.length() > longest.length() || dictword.compareTo(longest) < 0){
-                    longest = dictword;
+            if (i == dictWord.length() && dictWord.length() >= longest.length()){
+                if (dictWord.length() > longest.length() || dictWord.compareTo(longest) < 0 ){
+                    longest = dictWord;
                 }
             }
-
         }
-
         return longest;
+
     }
 }
