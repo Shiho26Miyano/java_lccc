c8e8e68910fda9f8032cb6b2338f2a9aabdcf7bf
Elain Hu <besthynlove@gmail.com>
2022-11-26 10:59:13 -0500

    m

--- src/com/company/lc_524_Longest_Word_in_Dictionary_through_Deleting.java
+++ src/com/company/lc_524_Longest_Word_in_Dictionary_through_Deleting.java
@@ -3,14 +3,16 @@ import java.util.*;
 
 public class lc_524_Longest_Word_in_Dictionary_through_Deleting {
     public String findLongestWord(String s, List<String> d) {
-        String res="";
-        for (String c: d)
-            if ((c.length()>res.length() || c.length()==res.length() && c.compareTo(res)<0) && isSubseq(c, s)) res=c;
-        return res;
-    }
-    public boolean isSubseq(String a, String b){
-        int i=-1, j=-1;
-        while (++i<a.length()) if ((j=b.indexOf(a.charAt(i), j+1))==-1) return false;
-        return true;
+        String longest = "";
+        for (String dictWord : d) {
+            int i = 0;
+            for (char c : s.toCharArray())
+                if (i < dictWord.length() && c == dictWord.charAt(i)) i++;
+
+            if (i == dictWord.length() && dictWord.length() >= longest.length())
+                if (dictWord.length() > longest.length() || dictWord.compareTo(longest) < 0)
+                    longest = dictWord;
+        }
+        return longest;
     }
 }
