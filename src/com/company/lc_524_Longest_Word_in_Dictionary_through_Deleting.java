21cbf47e57dcd26600b6727b8b7963b9b7e0cc12
Elain Hu <besthynlove@gmail.com>
2022-11-26 12:04:05 -0500

    m

--- src/com/company/lc_524_Longest_Word_in_Dictionary_through_Deleting.java
+++ src/com/company/lc_524_Longest_Word_in_Dictionary_through_Deleting.java
@@ -4,20 +4,21 @@ import java.util.*;
 public class lc_524_Longest_Word_in_Dictionary_through_Deleting {
     public String findLongestWord(String s, List<String> dictionary) {
         String longest = "";
-        for (String dictWord : dictionary){
+        for (String dictword: dictionary){
             int i = 0;
             for (char c : s.toCharArray()){
-                if (i < dictWord.length() && c == dictWord.charAt(i)){
+                if (i < dictword.length() && c == dictword.charAt(i)){
                     i++;
                 }
             }
-
-            if (i == dictWord.length() && dictWord.length() >= longest.length()){
-                if (dictWord.length() > longest.length() || dictWord.compareTo(longest) < 0){
-                    longest = dictWord;
+            if (i == dictword.length() && dictword.length() >= longest.length()){
+                if (dictword.length() > longest.length() || dictword.compareTo(longest) < 0){
+                    longest = dictword;
                 }
             }
+
         }
+
         return longest;
     }
 }
