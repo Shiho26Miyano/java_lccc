171c72486b425cf598b377fa1849ec9c38681888
Elain Hu <besthynlove@gmail.com>
2022-11-27 18:37:24 -0500

    m

--- src/com/company/lc_581_Shortest_Unsorted_Continuous_Subarray.java
+++ src/com/company/lc_581_Shortest_Unsorted_Continuous_Subarray.java
@@ -2,13 +2,13 @@ package com.company;
 import java.util.*;
 
 public class lc_581_Shortest_Unsorted_Continuous_Subarray {
-    public int findUnsortedSubarray(int[] A) {
-        int n = A.length, begin = -1, end = -2, min = A[n - 1], max = A[0];
+    public int findUnsortedSubarray(int[] nums) {
+        int n = nums.length, begin = -1, end = -2, min = nums[n - 1], max = nums[0];
         for (int i = 1; i < n; i++){
-            max = Math.max(max, A[i]);
-            min = Math.min(min, A[n - 1 - i]);
-            if (A[i] < max) end = i;
-            if (A[n - 1 - i] > min) begin = n - 1 - i;
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[n - i - 1]);
+            if (nums[i] < max) end = i;
+            if (nums[n - i - 1] > min) begin = n - i - 1;
         }
         return end - begin + 1;
 
