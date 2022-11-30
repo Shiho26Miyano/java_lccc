545797236fedc0624dfc807fad26aa40cef0fb41
Elain Hu <besthynlove@gmail.com>
2022-11-29 23:47:24 -0500

    m

---
+++ src/com/company/lc_611_Valid_Triangle_Number.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class lc_611_Valid_Triangle_Number {
+    public static int triangleNumber(int[] A) {
+        Arrays.sort(A);
+        int count = 0, n = A.length;
+        for (int i=n-1;i>=2;i--) {
+            int l = 0, r = i-1;
+            while (l < r) {
+                if (A[l] + A[r] > A[i]) {
+                    count += r-l;
+                    r--;
+                }
+                else l++;
+            }
+        }
+        return count;
+    }
+}
