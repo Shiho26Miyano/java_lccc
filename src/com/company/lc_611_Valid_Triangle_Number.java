9e988cbbf17b3c5ec25bfdb17ff40edbeb5b83ab
Elain Hu <besthynlove@gmail.com>
2022-12-03 16:13:49 -0500

    m

--- src/com/company/lc_611_Valid_Triangle_Number.java
+++ src/com/company/lc_611_Valid_Triangle_Number.java
@@ -2,14 +2,15 @@ package com.company;
 import java.util.*;
 
 public class lc_611_Valid_Triangle_Number {
-    public static int triangleNumber(int[] A) {
-        Arrays.sort(A);
-        int count = 0, n = A.length;
-        for (int i=n-1;i>=2;i--) {
-            int l = 0, r = i-1;
+    public static int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0, n = nums.length;
+        for (int i = n - 1; i >= 2; i--) {
+            int l = 0, r = i - 1;
             while (l < r) {
-                if (A[l] + A[r] > A[i]) {
-                    count += r-l;
+                if (nums[l] + nums[r] > nums[i]) {
+                    // it is r - l not r - 1
+                    count += r - l;
                     r--;
                 }
                 else l++;
