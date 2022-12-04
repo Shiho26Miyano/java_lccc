558761daae54953d6de0c3348b43cbc3748db8f4
Elain Hu <besthynlove@gmail.com>
2022-12-04 12:31:03 -0500

    m

--- src/com/company/lc_696_Count_Binary_Substrings.java
+++ src/com/company/lc_696_Count_Binary_Substrings.java
@@ -8,13 +8,19 @@ public class lc_696_Count_Binary_Substrings {
 
 
     public static int countBinarySubstrings(String s) {
-        if(s==null || s.length()<=1) return 0;
-        int res =0;
+        if (s==null || s.length()<=1){
+            return 0;
+        }
+        int res = 0;
         int preCnt = 0, curCnt = 1;
         char[] crs = s.toCharArray();
-        for(int i=1; i<crs.length; i++){
-            if(crs[i]==crs[i-1]) curCnt++;
-            else {preCnt = curCnt; curCnt = 1;}
+        for (int i = 1; i < crs.length; i++){
+            if (crs[i]==crs[i-1]){
+                curCnt++;
+            }else{
+                preCnt = curCnt;
+                curCnt = 1;
+            }
             if(preCnt>=curCnt) res++;
         }
         return res;
