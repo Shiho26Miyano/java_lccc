9fbca803634992b42a0f4574c8179c84003fdd51
Elain Hu <besthynlove@gmail.com>
2022-12-11 21:08:33 -0500

    m

--- src/com/company/lc_1048_Longest_String_Chain.java
+++ src/com/company/lc_1048_Longest_String_Chain.java
@@ -4,7 +4,7 @@ public class lc_1048_Longest_String_Chain {
     //https://leetcode.com/problems/longest-string-chain/solutions/294890/java-c-python-dp-solution/?orderBy=most_votes
     //Sort the words by word's length. (also can apply bucket sort)
     //For each word, loop on all possible previous word with 1 letter missing.
-    //If we have seen this previous word, update the longest chain for the current word.
+    //If we have seen this previous word, add the current word to the longest chain for the current word.
     //Finally return the longest word chain
     public int longestStrChain(String[] words) {
         Map<String, Integer> dp = new HashMap<>();
