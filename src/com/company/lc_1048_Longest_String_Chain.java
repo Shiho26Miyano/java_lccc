cc2699b0dcd4b168ea893da1ea6c9930cd3b3707
Elain Hu <besthynlove@gmail.com>
2022-12-18 23:11:30 -0500

    m

--- src/com/company/lc_1048_Longest_String_Chain.java
+++ src/com/company/lc_1048_Longest_String_Chain.java
@@ -7,19 +7,20 @@ public class lc_1048_Longest_String_Chain {
     //If we have seen this previous word, add the current word to the longest chain for the current word.
     //Finally return the longest word chain
     public int longestStrChain(String[] words) {
-        Map<String, Integer> dp = new HashMap<>();
+        Map<String, Integer>dp = new HashMap<>();
         Arrays.sort(words, (a, b)->a.length() - b.length());
         int res = 0;
         for (String word: words){
-            int best = 0 ;
+            int best = 0;
             for (int i = 0; i < word.length(); i++){
                 String prev = word.substring(0, i) + word.substring(i + 1);
-                best = Math.max(best, dp.getOrDefault(prev, 0) + 1);
+                best = Math.max(best, dp.getOrDefault(prev, 0 )+ 1);
             }
             dp.put(word, best);
             res = Math.max(res, best);
         }
         return res;
+
     }
 }
 
