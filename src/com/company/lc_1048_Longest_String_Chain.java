7d7628075e90013c8215eb2bec0b1d095b33b0ba
Elain Hu <besthynlove@gmail.com>
2023-01-01 11:40:05 -0500

    m

--- src/com/company/lc_1048_Longest_String_Chain.java
+++ src/com/company/lc_1048_Longest_String_Chain.java
@@ -10,19 +10,19 @@ public class lc_1048_Longest_String_Chain {
     //  We store this result in a map where key is the word and value is the sequence length. By doing this we ensure that, for each word that we encounter, we already know the result of all of its possible predecessors.
     public int longestStrChain(String[] words) {
         Map<String, Integer>map = new HashMap<>();
-        Arrays.sort(words, (a, b)-> a.length() - b.length());
+        Arrays.sort(words, (a, b)->a.length() - b.length());
         int res = 0;
         for (String word: words){
             int maxFreq = 0;
             for (int i = 0; i < word.length(); i++){
-                String curChar = word.substring(0, i) + word.substring(i + 1);
+                String misCharWord = word.substring(0, i) + word.substring(i + 1);
                 //calculate how many times the word with 1 missing char appears in the mapping list.
-                int freq = map.getOrDefault(curChar, 0) + 1;
+                int freq = map.getOrDefault(misCharWord, 0) + 1;
                 // in each word, find higest freq that missing 1 char word appear most
-                maxFreq = Math.max(freq, maxFreq);
+                maxFreq = Math.max(maxFreq, freq);
+
             }
             // put each word with its highest freq in the map
-
             map.put(word, maxFreq);
             // get the highest freq in the mapping
             res = Math.max(res, maxFreq);
