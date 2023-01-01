4e15e98e544bb21bc0ba63cacfbead28a729981e
Elain Hu <besthynlove@gmail.com>
2023-01-01 11:57:17 -0500

    m

---
+++ src/com/company/myPratice_1.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class myPratice_1 {
+
+    public static int recurSum(int k){
+        if (k > 0){
+            return k + recurSum(k - 1);
+        } else{
+            return 0;
+        }
+    }
+    public static void main(String[] args){
+        int result = recurSum(5);
+        System.out.println((result));
+    }
+}
