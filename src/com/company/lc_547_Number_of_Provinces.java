229b9607f5aa8f4399103dd4aae4cf9349930dd5
Elain Hu <besthynlove@gmail.com>
2023-01-02 12:39:13 -0500

    m

--- src/com/company/lc_547_Number_of_Provinces.java
+++ src/com/company/lc_547_Number_of_Provinces.java
@@ -2,6 +2,7 @@ package com.company;
 import java.util.*;
 
 public class lc_547_Number_of_Provinces {
+    // m[i][i] is always 1 because it represents ith city. M[i][j] represents the relation betweeh ith city and jth city
     public void dfs(int[][] M, int[] visited, int i) {
         for (int j = 0; j < M.length; j++) {
             if (M[i][j] == 1 && visited[j] == 0) {
