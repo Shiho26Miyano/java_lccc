b0ed68428d4c4a68c2fb210bc88cef71f606a7e1
Elain Hu <besthynlove@gmail.com>
2023-01-08 12:38:06 -0500

    m

--- src/com/company/lc_326_Power_of_Three.java
+++ src/com/company/lc_326_Power_of_Three.java
@@ -1,4 +1,15 @@
 package com.company;
 
 public class lc_326_Power_of_Three {
+
+    public boolean isPowerOfThree2(int n) {
+        if(n == 0 ) return false;
+        while (n != 1){
+            int r = n % 3;
+            n = n /3;
+            if (r != 0)
+                return false;
+        }
+        return true;
+    }
 }
