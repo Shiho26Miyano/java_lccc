b50879dd514e18889476242073389019fbb18207
Elain Hu <besthynlove@gmail.com>
2023-01-15 11:18:12 -0500

    m

--- src/com/company/lc_390_Elimination_Game.java
+++ src/com/company/lc_390_Elimination_Game.java
@@ -15,6 +15,14 @@ package com.company;
 //left(2n)=2(n-left(n)+1)
 //left(n)=2(n/2 -left(n/2)+1)
 
+//-----
+//Everytime our "n" is getting halved.
+//We notice that if we take out 2 common, all the patterns are same
+//First Step : 1,2,3,4,5,6,7,8,9
+//Second Step : 2 x [1,2,3,4]
+//Third Step : 2x [1,3]
+//We just need to take care about the iteration with odd length, so we add a 1 in the end. Odd+1=Even.
+
 public class lc_390_Elimination_Game {
     public int lastRemaining(int n) {
         if(n==1)
