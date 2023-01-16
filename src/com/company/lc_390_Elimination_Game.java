90084d57e45ce3907a2cd0e5908f5fc947d8e1c6
Elain Hu <besthynlove@gmail.com>
2023-01-16 16:53:49 -0500

    m

--- src/com/company/lc_390_Elimination_Game.java
+++ src/com/company/lc_390_Elimination_Game.java
@@ -25,9 +25,8 @@ package com.company;
 
 public class lc_390_Elimination_Game {
     public int lastRemaining(int n) {
-        if(n==1)
-            return n;
-        return 2*(n/2-lastRemaining(n/2) + 1);
+        if (n == 1) return n;
+        return 2 * (n / 2 - lastRemaining( n / 2) + 1);
 
     }
 }
