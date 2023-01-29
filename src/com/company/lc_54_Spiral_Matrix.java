9d47f2e02733802dc1b3cdae66c34f30db75570b
Elain Hu <besthynlove@gmail.com>
2023-01-29 16:51:54 -0500

    m

--- src/com/company/lc_54_Spiral_Matrix.java
+++ src/com/company/lc_54_Spiral_Matrix.java
@@ -3,44 +3,26 @@ import java.util.*;
 
 public class lc_54_Spiral_Matrix {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result = new ArrayList<>();
-        int rows = matrix.length;
-        int columns = matrix[0].length;
-        int up = 0;
-        int left = 0;
-        int right = columns - 1;
-        int down = rows - 1;
+        List<Integer> res = new LinkedList<>();
+        if (matrix == null || matrix.length == 0) return res;
+        int n = matrix.length, m = matrix[0].length;
+        int up = 0,  down = n - 1;
+        int left = 0, right = m - 1;
+        while (res.size() < n * m) {
+            for (int j = left; j <= right && res.size() < n * m; j++)
+                res.add(matrix[up][j]);
 
-        while (result.size() < rows * columns) {
-            // Traverse from left to right.
-            for (int col = left; col <= right; col++) {
-                result.add(matrix[up][col]);
-            }
-            // Traverse downwards.
-            for (int row = up + 1; row <= down; row++) {
-                result.add(matrix[row][right]);
-            }
-            // Make sure we are now on a different row.
-            if (up != down) {
-                // Traverse from right to left.
-                for (int col = right - 1; col >= left; col--) {
-                    result.add(matrix[down][col]);
-                }
-            }
-            // Make sure we are now on a different column.
-            if (left != right) {
-                // Traverse upwards.
-                for (int row = down - 1; row > up; row--) {
-                    result.add(matrix[row][left]);
-                }
-            }
-            left++;
-            right--;
-            up++;
-            down--;
+            for (int i = up + 1; i <= down - 1 && res.size() < n * m; i++)
+                res.add(matrix[i][right]);
+
+            for (int j = right; j >= left && res.size() < n * m; j--)
+                res.add(matrix[down][j]);
+
+            for (int i = down - 1; i >= up + 1 && res.size() < n * m; i--)
+                res.add(matrix[i][left]);
+
+            left++; right--; up++; down--;
         }
-
-        return result;
-    }
+        return res;
     }
 }
