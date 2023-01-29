0bd9d53b68a885896ccc9ff6a7e9950a4a78a9d7
Elain Hu <besthynlove@gmail.com>
2023-01-29 16:53:44 -0500

    m

--- src/com/company/lc_54_Spiral_Matrix.java
+++ src/com/company/lc_54_Spiral_Matrix.java
@@ -5,20 +5,20 @@ public class lc_54_Spiral_Matrix {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> res = new LinkedList<>();
         if (matrix == null || matrix.length == 0) return res;
-        int n = matrix.length, m = matrix[0].length;
-        int up = 0,  down = n - 1;
-        int left = 0, right = m - 1;
-        while (res.size() < n * m) {
-            for (int j = left; j <= right && res.size() < n * m; j++)
+        int row = matrix.length, col = matrix[0].length;
+        int up = 0,  down = row - 1;
+        int left = 0, right = col - 1;
+        while (res.size() < row * col) {
+            for (int j = left; j <= right && res.size() < row * col; j++)
                 res.add(matrix[up][j]);
 
-            for (int i = up + 1; i <= down - 1 && res.size() < n * m; i++)
+            for (int i = up + 1; i <= down - 1 && res.size() < row * col; i++)
                 res.add(matrix[i][right]);
 
-            for (int j = right; j >= left && res.size() < n * m; j--)
+            for (int j = right; j >= left && res.size() < row * col; j--)
                 res.add(matrix[down][j]);
 
-            for (int i = down - 1; i >= up + 1 && res.size() < n * m; i--)
+            for (int i = down - 1; i >= up + 1 && res.size() < row * col; i--)
                 res.add(matrix[i][left]);
 
             left++; right--; up++; down--;
