26d48adf74117945b0b52ad7ac2fb1f217f96f73
Elain Hu <besthynlove@gmail.com>
2023-01-29 18:11:33 -0500

    m

--- src/com/company/lc_54_Spiral_Matrix.java
+++ src/com/company/lc_54_Spiral_Matrix.java
@@ -9,21 +9,20 @@ public class lc_54_Spiral_Matrix {
         int top = 0, bottom = rows - 1;
         int left = 0, right = cols - 1;
 
-        while (res.size() < rows * cols ){
+        while (res.size() < rows * cols){
             for (int a = left; a <= right && res.size() < rows * cols; a++)
                 res.add(matrix[top][a]);
-
             for (int b = top + 1; b <= bottom - 1 && res.size() < rows * cols; b++)
                 res.add(matrix[b][right]);
-
             for (int c = right; c >= left && res.size() < rows * cols; c--)
                 res.add(matrix[bottom][c]);
-
-            for (int d = bottom - 1; d >=top + 1 && res.size() < rows * cols; d--)
+            for(int d = bottom - 1; d >= top + 1 && res.size() < rows * cols; d--)
                 res.add(matrix[d][left]);
+            left++; top++; right--; bottom--;
 
-            left++; right--; top++; bottom--;
         }
+
         return res;
+
     }
 }
