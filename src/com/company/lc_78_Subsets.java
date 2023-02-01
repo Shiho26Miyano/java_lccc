1d1ea9e50e96ffc7037aae7e15fe7eaf0950b098
Elain Hu <besthynlove@gmail.com>
2023-01-31 23:35:17 -0500

    m

--- src/com/company/lc_78_Subsets.java
+++ src/com/company/lc_78_Subsets.java
@@ -3,15 +3,18 @@ import java.util.*;
 
 //backtracking
 public class lc_78_Subsets {
-    List<List<Integer>> output = new ArrayList();
+    List<List<Integer>> output = new ArrayList<>();
     int n, k;
+
     public void backtrack(int first, ArrayList<Integer> curr, int[] nums) {
         // if the combination is done
         if (curr.size() == k) {
             output.add(new ArrayList(curr));
             return;
         }
-        for (int i = first; i < n; ++i) {
+
+
+        for (int i = first; i < n; i++){
             // add i into the current combination
             curr.add(nums[i]);
             // use next integers to complete the combination
@@ -20,9 +23,9 @@ public class lc_78_Subsets {
             curr.remove(curr.size() - 1);
         }
     }
-    public List<List<Integer>> subsets(int[] nums) {
+    public List<List<Integer>> subsets(int[] nums){
         n = nums.length;
-        for (k = 0; k < n + 1; ++k) {
+        for (k = 0; k < n + 1; k++){
             backtrack(0, new ArrayList<Integer>(), nums);
         }
         return output;
