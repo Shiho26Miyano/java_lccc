c5bd1cf6796fb72b77c065fb8244944b488ce399
Elain Hu <besthynlove@gmail.com>
2023-02-05 21:54:46 -0500

    m

--- src/com/company/lc_78_Subsets.java
+++ src/com/company/lc_78_Subsets.java
@@ -5,6 +5,15 @@ import java.util.*;
 // Backtracking is an algorithm for finding all solutions by exploring all potential candidates.
 // If the solution candidate turns to be not a solution (or at least not the last one),
 // backtracking algorithm discards it by making some changes on the previous step, i.e. backtracks and then try again.
+
+// to do
+// define a backtrack function backtrack(first, curr_combination, nums arraylist)
+// if the current combination is done, we will add the combination to the final output
+// otherwise, we will iterate over the idexes i from first to the length of the entire sequence n
+// add integer nums[i] into the current combination curr
+// proceed to add more integers into the combination: backtrack(i + 1, curr)
+// backtrack by removing nums[i] from curr.
+
 public class lc_78_Subsets {
     List<List<Integer>> output = new ArrayList<>();
     int n, k;
