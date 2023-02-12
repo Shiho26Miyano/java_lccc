87483388d4382e271beef81a85b58be50535092f
Elain Hu <besthynlove@gmail.com>
2023-02-12 18:20:13 -0500

    m

--- src/com/company/lc_1265_Print_Immutable_Linked_List_in_Reverse.java
+++ src/com/company/lc_1265_Print_Immutable_Linked_List_in_Reverse.java
@@ -1,13 +1,26 @@
 package com.company;
+import java.util.*;
+
 
 public class lc_1265_Print_Immutable_Linked_List_in_Reverse {
     //1 Iterative - recursion
-    public void printLinkedListInReverse(ImmutableListNode head) {
+    public void printLinkedListInReverse1(ImmutableListNode head) {
         if (head == null) {
             return;
         }
-        printLinkedListInReverse(head.getNext());
+        printLinkedListInReverse1(head.getNext());
         head.printValue();
+    }
+    // 2 Iterative - stack
+    public void printLinkedListInReverse2(ImmutableListNode head) {
+        Stack<ImmutableListNode> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head);
+            head = head.getNext();
+        }
+        while (!stack.isEmpty()) {
+            stack.pop().printValue();
+        }
     }
 }
 
