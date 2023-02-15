76f1ef574831d2d4a94e7de23df1a3da9b45df68
Elain Hu <besthynlove@gmail.com>
2023-02-15 00:01:42 -0500

    m

---
+++ src/com/company/lc_2130_Maximum_Twin_Sum_of_a_Linked_List.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class lc_2130_Maximum_Twin_Sum_of_a_Linked_List {
+    int maxSum = Integer.MIN_VALUE;
+    ListNode nodePtr = null;
+
+    public int pairSum(ListNode head) {
+        nodePtr = head;
+        traverse(head);
+        return maxSum;
+    }
+
+    public void traverse(ListNode node) {
+        if (node == null) return;
+        if (node.next == nodePtr) return;
+        traverse(node.next);
+        maxSum = Math.max(maxSum, nodePtr.val + node.val);
+        nodePtr = nodePtr.next;
+    }
+}
