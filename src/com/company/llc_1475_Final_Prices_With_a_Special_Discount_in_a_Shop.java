9c63056364fbbeea900699b27a7d90f602d74693
Elain Hu <besthynlove@gmail.com>
2023-02-20 11:40:43 -0500

    m

---
+++ src/com/company/llc_1475_Final_Prices_With_a_Special_Discount_in_a_Shop.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class llc_1475_Final_Prices_With_a_Special_Discount_in_a_Shop {
+    public int[] finalPrices(int[] A) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < A.length; i++) {
+            while (!stack.isEmpty() && A[stack.peek()] >= A[i])
+                A[stack.pop()] -= A[i];
+            stack.push(i);
+        }
+        return A;
+    }
+}
