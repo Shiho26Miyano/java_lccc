d0d2d691d366a1e3131141cb2c5743cd245040ef
Elain Hu <besthynlove@gmail.com>
2023-02-20 12:27:31 -0500

    m

--- src/com/company/lc_2130_Maximum_Twin_Sum_of_a_Linked_List.java
+++ src/com/company/lc_2130_Maximum_Twin_Sum_of_a_Linked_List.java
@@ -18,6 +18,24 @@ public class lc_2130_Maximum_Twin_Sum_of_a_Linked_List {
         nodePtr = nodePtr.next;
         return nodePtr;
     }
+
+
+
+////https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/solutions/2846367/java-recursive-approach-o-n-easy-to-understand-clean-solution/
+
+    public int pairSum2(ListNode head) {
+        nodePtr = head;
+        traverse2(head);
+        return maxSum;
+    }
+
+    public void traverse2(ListNode node) {
+        if (node == null) return;
+        if (node.next == nodePtr) return;
+        traverse2(node.next);
+        maxSum = Math.max(maxSum, nodePtr.val + node.val);
+        nodePtr = nodePtr.next;
+    }
 }
 
 
