91197cbf7b315a5dfd69ae60496e166c4b1dbde0
Elain Hu <besthynlove@gmail.com>
2023-02-24 17:05:50 -0500

    m

--- src/com/company/lc_2130_Maximum_Twin_Sum_of_a_Linked_List.java
+++ src/com/company/lc_2130_Maximum_Twin_Sum_of_a_Linked_List.java
@@ -1,38 +1,26 @@
 package com.company;
 
 public class lc_2130_Maximum_Twin_Sum_of_a_Linked_List {
-    int maxSum = Integer.MIN_VALUE;
-    ListNode nodePtr = null;
 
-    public int pairSum(ListNode head) {
-        nodePtr = head;
-        traverse(head);
-        return maxSum;
-    }
-
-    public ListNode traverse(ListNode node) {
-        if (node == null) return null;
-        if (node.next == nodePtr) return null;
-        traverse(node.next);
-        maxSum = Math.max(maxSum, nodePtr.val + node.val);
-        nodePtr = nodePtr.next;
-        return nodePtr;
-    }
-    
 ////https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/solutions/2846367/java-recursive-approach-o-n-easy-to-understand-clean-solution/
 
-    public int pairSum2(ListNode head) {
-        nodePtr = head;
-        traverse2(head);
+
+    int maxSum = 0;
+    ListNode nodePair = null;
+    public int pairSum(ListNode head) {
+
+        nodePair = head;
+        traverse(nodePair);
         return maxSum;
+
     }
 
-    public void traverse2(ListNode node) {
+    public void traverse(ListNode node){
         if (node == null) return;
-        if (node.next == nodePtr) return;
-        traverse2(node.next);
-        maxSum = Math.max(maxSum, nodePtr.val + node.val);
-        nodePtr = nodePtr.next;
+        if (node.next == nodePair) return;
+        traverse(node.next);
+        maxSum = Math.max(maxSum, nodePair.val + node.val);
+        nodePair = nodePair.next;
     }
 }
 
