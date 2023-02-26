b422e15f3c70b273262254ae5209bcf9e44ac7dc
Elain Hu <besthynlove@gmail.com>
2023-02-26 16:33:49 -0500

    m

--- src/com/company/lc_371_Sum_of_Two_Integers.java
+++ src/com/company/lc_371_Sum_of_Two_Integers.java
@@ -2,10 +2,13 @@ package com.company;
 
 //https://leetcode.com/problems/sum-of-two-integers/solutions/2562077/beginner-level-explanation-with-code/
 //https://leetcode.com/problems/sum-of-two-integers/solutions/84277/one-liner-with-detailed-explanation/
+//https://www.geeksforgeeks.org/bitwise-operators-in-java/
 public class lc_371_Sum_of_Two_Integers {
     public static int getSum(int a, int b) {
         if (a == 0) return b;
         if (b == 0) return a;
-        return getSum(a ^ b, (a & b) << 1);
+        int carry = (a & b) << 1;
+        int sum = a ^ b;
+        return getSum(sum, carry);
     }
 }
