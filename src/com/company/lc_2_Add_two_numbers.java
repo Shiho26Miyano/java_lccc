901fcd7d679026cc31e6c02057259ff9599691d4
Elain Hu <besthynlove@gmail.com>
2023-02-26 11:12:56 -0500

    m

--- src/com/company/lc_2_Add_two_numbers.java
+++ src/com/company/lc_2_Add_two_numbers.java
@@ -14,26 +14,30 @@ public class lc_2_Add_two_numbers {
      public int getValue() {return value;}
   }
 
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode tail = new ListNode(0);
-        ListNode dummy = tail;
-        int sum = 0;
-        while (l1 != null || l2 != null || sum > 0) {
-            sum += (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
-            tail.next = new ListNode(sum % 10);
-            tail = tail.next;
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
-            sum /= 10;
+    int carry = 0;
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        if (l1 == null && l2 == null && carry == 0) {
+            return null;
         }
-        return dummy.next;
+
+        int val1 = l1 == null ? 0 : l1.val;
+        int val2 = l2 == null ? 0 : l2.val;
+
+        int sum = val1 + val2 + carry;
+        carry = sum/10;
+
+        l1 = l1 == null ? null : l1.next;
+        l2 = l2 == null ? null : l2.next;
+
+        ListNode ans = new ListNode(sum%10, addTwoNumbers(l1, l2));
+
+        return ans;
     }
 
     public static void main(String[] args){
         ListNode l1 = new ListNode(8);
-
-        
-
         System.out.println(l1.getValue());
 
     }
