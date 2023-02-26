421f644ec16a16e88367f90c984e247ee5b3f4d9
Elain Hu <besthynlove@gmail.com>
2023-02-26 16:15:04 -0500

    m

--- src/com/company/lc_371_Sum_of_Two_Integers.java
+++ src/com/company/lc_371_Sum_of_Two_Integers.java
@@ -1,6 +1,7 @@
 package com.company;
 
 //https://leetcode.com/problems/sum-of-two-integers/solutions/2562077/beginner-level-explanation-with-code/
+//https://leetcode.com/problems/sum-of-two-integers/solutions/84277/one-liner-with-detailed-explanation/
 public class lc_371_Sum_of_Two_Integers {
     public static int getSum(int a, int b) {
         if (a == 0) return b;
