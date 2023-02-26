d87c3d82ef38f28220179992ccede851e0bfe013
Elain Hu <besthynlove@gmail.com>
2023-02-26 11:30:28 -0500

    m

--- src/com/company/lc_2_Add_two_numbers.java
+++ src/com/company/lc_2_Add_two_numbers.java
@@ -18,22 +18,19 @@ public class lc_2_Add_two_numbers {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        if (l1 == null && l2 == null && carry == 0) {
-            return null;
-        }
+        if (l1 == null && l2 == null && carry == 0) return null;
+
 
         int val1 = l1 == null ? 0 : l1.val;
         int val2 = l2 == null ? 0 : l2.val;
 
         int sum = val1 + val2 + carry;
-        carry = sum/10;
+        carry = sum / 10;
+        ListNode l1Next = l1 == null ? null : l1.next;
+        ListNode l2Next = l2 == null ? null : l2.next;
 
-        l1 = l1 == null ? null : l1.next;
-        l2 = l2 == null ? null : l2.next;
+        return (new ListNode(sum % 10, addTwoNumbers(l1Next, l2Next)));
 
-        ListNode ans = new ListNode(sum%10, addTwoNumbers(l1, l2));
-
-        return ans;
     }
 
     public static void main(String[] args){
