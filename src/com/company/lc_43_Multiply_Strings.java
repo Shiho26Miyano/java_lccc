aaa777936c5a77c7c01d7367396a547db36aff6e
Elain Hu <besthynlove@gmail.com>
2023-02-26 23:31:17 -0500

    m

--- src/com/company/lc_43_Multiply_Strings.java
+++
@@ -1,4 +0,0 @@
-package com.company;
-
-public class lc_43_Multiply_Strings {
-}
---
+++ src/com/company/lc_817_Linked_List_Components.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class lc_817_Linked_List_Components {
+    public int numComponents(ListNode head, int[] G) {
+        Set<Integer> setG = new HashSet<>();
+        for (int i: G) setG.add(i);
+        int res = 0;
+        while (head != null) {
+            if (setG.contains(head.val) && (head.next == null || !setG.contains(head.next.val))) res++;
+            head = head.next;
+        }
+        return res;
+    }
+}
