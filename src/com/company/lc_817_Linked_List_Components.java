9c380e6706e54e0ac0db8e42cf38d6f390b85572
Elain Hu <besthynlove@gmail.com>
2023-02-27 08:16:28 -0500

    m

--- src/com/company/lc_817_Linked_List_Components.java
+++ src/com/company/lc_817_Linked_List_Components.java
@@ -2,17 +2,17 @@ package com.company;
 import java.util.*;
 
 public class lc_817_Linked_List_Components {
-    public int numComponents(ListNode head, int[] G) {
-        Set<Integer> setG = new HashSet<>();
-        for (int i: G) setG.add(i);
+    public int numComponents(ListNode head, int[] nums) {
+        Set<Integer> hashset = new HashSet<>();
+        for (int i : nums) hashset.add(i);
         int res = 0;
-        while (head != null) {
-            if (setG.contains(head.val) && (head.next == null || !setG.contains(head.next.val))) res++;
+        while(head != null){
+            if (hashset.contains(head.val) && (head.next == null || !hashset.contains(head.next.val))) res++;
             head = head.next;
         }
         return res;
+
     }
-    
 }
 
 
