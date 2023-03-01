ee39935896b985e7c5a0cb2e1fa0efda79b9d71a
Elain Hu <besthynlove@gmail.com>
2023-02-28 21:58:10 -0500

    m

--- src/com/company/lc_2095_Delete_the_Middle_Node_of_a_Linked_List.java
+++ src/com/company/lc_2095_Delete_the_Middle_Node_of_a_Linked_List.java
@@ -13,6 +13,11 @@ public class lc_2095_Delete_the_Middle_Node_of_a_Linked_List {
             fast = fast.next.next;
 
         }
+
+
+        // prev slow is the pointer which is 1 behind the middle point
+        // so for e.g - [1,2,3,4,5] (odd length case) = when slow reaches 3, prev_slow is at 2 and we can delete 3 by 2.next = 3.next, i.e - [2 --> 4]
+        // similarly - [1, 2, 3, 4] (even length case) = when slow reaches 3, prev_slow is at 2 and we can delete 3 by 2.next = 3.next, i.e - [2 --> 4]
         preSlow.next = slow.next;
         return head;
 
