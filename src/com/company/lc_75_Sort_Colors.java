f490eeed20436d01352544742befe75902246518
Elain Hu <besthynlove@gmail.com>
2023-03-05 11:34:58 -0500

    m

---
+++ src/com/company/lc_75_Sort_Colors.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.*;
+
+public class lc_75_Sort_Colors {
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length-1;
+
+        while(mid <= high){
+            if(nums[mid] == 0 ){
+                //swap with left
+                swap(nums,low, mid );
+                low++;
+                mid++;
+            }else if(nums[mid] == 2){
+                swap(nums, mid, high);
+                high--;
+            }else{
+                mid++;
+            }
+        }
+    }
+
+    public static void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
