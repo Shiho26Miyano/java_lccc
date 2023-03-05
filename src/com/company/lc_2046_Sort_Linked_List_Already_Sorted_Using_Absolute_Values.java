d9522de6890fbde9f628b69c66ad7b210a8951b8
Elain Hu <besthynlove@gmail.com>
2023-03-05 10:42:19 -0500

    m

--- src/com/company/lc_2046_Sort_Linked_List_Already_Sorted_Using_Absolute_Values.java
+++ src/com/company/lc_2046_Sort_Linked_List_Already_Sorted_Using_Absolute_Values.java
@@ -3,10 +3,10 @@ import java.util.*;
 
 public class lc_2046_Sort_Linked_List_Already_Sorted_Using_Absolute_Values {
     public ListNode sortLinkedList(ListNode head) {
-        var node = head;
+        ListNode node = head;
         while (node != null && node.next != null) {
             if (node.next.val < 0) {
-                var tempNodeNextNext = node.next.next;
+                ListNode tempNodeNextNext = node.next.next;
                 node.next.next = head;
                 head = node.next;
                 node.next = tempNodeNextNext;
