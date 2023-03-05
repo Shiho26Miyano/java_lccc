1e8df963fb962da9056d3a5b12d87fb2e9153c90
Elain Hu <besthynlove@gmail.com>
2023-03-05 11:13:35 -0500

    m

--- src/com/company/lc_2046_Sort_Linked_List_Already_Sorted_Using_Absolute_Values.java
+++ src/com/company/lc_2046_Sort_Linked_List_Already_Sorted_Using_Absolute_Values.java
@@ -6,9 +6,13 @@ public class lc_2046_Sort_Linked_List_Already_Sorted_Using_Absolute_Values {
         ListNode node = head;
         while (node != null && node.next != null) {
             if (node.next.val < 0) {
+                //save the node.next.next
                 ListNode tempNodeNextNext = node.next.next;
+                //push node.next.next to current head
                 node.next.next = head;
+                //push head to node.next which is the negative node
                 head = node.next;
+                // push node.next is the old node.next.next so negative node.next is skipped
                 node.next = tempNodeNextNext;
             } else {
                 node = node.next;
