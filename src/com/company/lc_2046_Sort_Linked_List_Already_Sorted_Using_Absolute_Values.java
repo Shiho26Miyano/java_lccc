2911f18faf47270730c727973f5467518736f6b9
Elain Hu <besthynlove@gmail.com>
2023-03-05 11:24:01 -0500

    m

--- src/com/company/lc_2046_Sort_Linked_List_Already_Sorted_Using_Absolute_Values.java
+++ src/com/company/lc_2046_Sort_Linked_List_Already_Sorted_Using_Absolute_Values.java
@@ -8,7 +8,7 @@ public class lc_2046_Sort_Linked_List_Already_Sorted_Using_Absolute_Values {
             if (node.next.val < 0) {
                 //save the node.next.next
                 ListNode tempNodeNextNext = node.next.next;
-                //below two steps 1.make node.next.next linked to head 2.move head to node.next, eg: head 0 moved to head -5
+                //below two steps 1.make node.next.next linked to head 2.move head to node.next, eg: head 0 moved to head -5; head move from 0 to 5, move to left/negative
                 node.next.next = head;
                 head = node.next;
                 // push node.next is the old node.next.next so negative node.next is skipped
