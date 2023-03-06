d9e797af4f711ca5bcf18bd04578dc27ca27229e
Elain Hu <besthynlove@gmail.com>
2023-03-05 20:36:03 -0500

    m

--- src/com/company/lc_280_Wiggle_Sort.java
+++ src/com/company/lc_280_Wiggle_Sort.java
@@ -3,15 +3,22 @@ import java.util.*;
 
 public class lc_280_Wiggle_Sort {
     public void wiggleSort(int[] nums) {
-        for(int i=0;i<nums.length;i++)
-            if(i%2==1){
-                if(nums[i-1]>nums[i]) swap(nums, i);
-            }else if(i!=0 && nums[i-1]<nums[i]) swap(nums, i);
+
+        for (int i = 0; i < nums.length; i++){
+            if(i%2 == 1){
+                if ((nums[i - 1] > nums[i])){
+                    swap(nums, i);
+                }
+
+            } else if (i != 0 && nums[i - 1] < nums[i]){
+                swap(nums, i);
+            }
+
+        }
     }
     public void swap(int[] nums, int i){
         int tmp=nums[i];
-        nums[i]=nums[i-1];
+        nums[i]=nums[i - 1];
         nums[i-1]=tmp;
     }
-
 }
