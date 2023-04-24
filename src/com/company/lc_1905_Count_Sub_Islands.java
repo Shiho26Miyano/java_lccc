1536feea6f35dba0d39aa87b99ba86bda1c2a908
Elain Hu <besthynlove@gmail.com>
2023-04-24 01:04:35 -0400

    m

--- src/com/company/lc_1905_Count_Sub_Islands.java
+++ src/com/company/lc_1905_Count_Sub_Islands.java
@@ -2,24 +2,32 @@ package com.company;
 import java.util.*;
 
 public class lc_1905_Count_Sub_Islands {
-    public int countSubIslands(int[][] B, int[][] A) {
-        int m = A.length, n = A[0].length, res = 0;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (A[i][j] == 1)
-                    res += dfs(B, A, i, j);
-        return res;
+    int ans =0;
+    public void dfs(int[][] grid1, int[][] grid2, int i , int j){
+        if(i<0|| i>=grid1.length ||j<0|| j>=grid1[0].length || grid2[i][j]==0 ) return;
+        if(grid1[i][j]== 0)ans =0;
+        grid2[i][j] =0;
+        dfs(grid1, grid2, i-1, j);
+        dfs(grid1, grid2, i+1, j);
+        dfs(grid1, grid2, i, j+1);
+        dfs(grid1, grid2, i, j-1);
     }
 
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        // problem can be solved using dfs
+        int res=0;
+        for(int i=0 ;i <grid1.length ;i++){
+            for(int j=0 ;j<grid2[0].length ;j++){
 
-    private int dfs(int[][] B, int[][] A, int i, int j) {
-        int m = A.length, n = A[0].length, res = 1;
-        if (i < 0 || i == m || j < 0 || j == n || A[i][j] == 0) return 1;
-        A[i][j] = 0;
-        res &= dfs(B, A, i - 1, j);
-        res &= dfs(B, A, i + 1, j);
-        res &= dfs(B, A, i, j - 1);
-        res &= dfs(B, A, i, j + 1);
-        return res & B[i][j];
+                if(grid2[i][j]==1)
+                {
+                    ans=1;
+                    dfs(grid1, grid2, i, j);
+                    res+=ans;
+                }
+            }
+        }
+        return res;
+
     }
 }
