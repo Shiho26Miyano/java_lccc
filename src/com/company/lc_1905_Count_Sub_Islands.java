386f3a9c5fbecafd2bf6e803e2d46664d4f2ce97
Elain Hu <besthynlove@gmail.com>
2023-04-24 23:39:21 -0400

    m

--- src/com/company/lc_1905_Count_Sub_Islands.java
+++ src/com/company/lc_1905_Count_Sub_Islands.java
@@ -7,7 +7,7 @@ public class lc_1905_Count_Sub_Islands {
     public int countSubIslands(int[][] grid1, int[][] grid2) {
         int res = 0;
         for (int i = 0; i < grid1.length; i++) {
-            for (int j = 0; j < grid2[0].length; j++) {
+            for (int j = 0; j < grid1[0].length; j++) {
                 if (grid2[i][j] == 1) {
                     ans = 1;
                     dfs(grid1, grid2, i, j);
