ac789e56214cba627a3ae66ce2bb3d6ddfbca3c9
Elain Hu <besthynlove@gmail.com>
2023-04-26 22:58:26 -0400

    m

--- src/com/company/lc_286_Walls_and_Gates.java
+++ src/com/company/lc_286_Walls_and_Gates.java
@@ -13,8 +13,7 @@ public class lc_286_Walls_and_Gates {
     }
 
     private void dfs(int[][] rooms, int i, int j, int dist) {
-        if(i < 0 || i >= rooms.length || j < 0 || j >= rooms[0].length) return;
-        if(rooms[i][j] < dist) return; // 0, -1, smaller distance already filled before
+        if(i < 0 || i >= rooms.length || j < 0 || j >= rooms[0].length || rooms[i][j] <= dist && dist != 0) return;
         rooms[i][j] = dist; //Only case left: INF
         dfs(rooms, i + 1, j, dist + 1);
         dfs(rooms, i - 1, j, dist + 1);
