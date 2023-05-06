984427674a94a2fa35f5e7e94d40c859702f87e2
Elain Hu <besthynlove@gmail.com>
2023-05-06 12:16:05 -0400

    m

--- src/com/company/lc_286_Walls_and_Gates.java
+++ src/com/company/lc_286_Walls_and_Gates.java
@@ -11,6 +11,7 @@ public class lc_286_Walls_and_Gates {
             }
         }
     }
+    // we want to exclude 1.i is out of range 2. j is out of range 3. roomms[i][j] already hit the min distance until now
     private void dfs(int[][] rooms, int i, int j, int dist) {
         if(i < 0 || i >= rooms.length || j < 0 || j >= rooms[0].length || rooms[i][j] <= dist && dist != 0) return;
         rooms[i][j] = dist; //Only case left: INF
