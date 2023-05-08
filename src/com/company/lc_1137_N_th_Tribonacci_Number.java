0fdbbfed6df307cf91aadb9edd35dfd6e7d74cca
Elain Hu <besthynlove@gmail.com>
2023-05-08 09:49:59 -0400

    m

--- src/com/company/lc_1137_N_th_Tribonacci_Number.java
+++ src/com/company/lc_1137_N_th_Tribonacci_Number.java
@@ -3,15 +3,14 @@ package com.company;
 public class lc_1137_N_th_Tribonacci_Number {
     /////////////method 1
     public int tribonacci(int n) {
-        return addTrib(n, 0, 1, 1);
-    }
+        return helper (n, 0, 1, 1);
 
-    private int addTrib(int n, int b1, int b2, int b3) {
+    }
+    private int helper(int n, int i0, int i1, int i2){
         if (n == 0) return 0;
         if (n == 1 || n == 2) return 1;
-        int sum = b1+b2+b3;
-        if (n == 3) return sum;
-        return addTrib(n-1, b2, b3, sum);
+        if (n == 3) return i0 + i1 + i2;
+        return helper (n - 1, i1, i2, i0 + i1 + i2);
     }
     ///////////method 2
     public int tribonacci_dp(int n) {
