3b681ce66a1332fbbff0d6aaafe58e7e5916ead2
Elain Hu <besthynlove@gmail.com>
2023-05-07 23:12:03 -0400

    m

---
+++ src/com/company/lc_430_Flatten_a_Multilevel_Doubly_Linked_List.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class lc_430_Flatten_a_Multilevel_Doubly_Linked_List {
+
+    Node430 prev = null;
+
+    public Node430 flatten(Node430 head) {
+        dfsHelper(head);
+        return head;
+    }
+
+    public void dfsHelper(Node430 current) {
+        if (current == null) return;
+        // postorder traversal, going right first or next in this case
+        dfsHelper(current.next);
+        dfsHelper(current.child);
+        // don't forget to set prev.prev pointer
+        if (prev != null) prev.prev = current;
+        // see explanation below
+        current.next = prev;
+        current.child = null;
+        prev = current;
+    }
+}
+class Node430 {
+    public int val;
+    public Node430 prev;
+    public Node430 next;
+    public Node430 child;
+};
