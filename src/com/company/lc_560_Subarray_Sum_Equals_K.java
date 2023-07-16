d06f99201ebfaa7b74737165a114a1f2ae5a7e6b
Elain Hu <besthynlove@gmail.com>
2023-07-16 16:50:06 -0400

    m

--- src/com/company/lc_560_Subarray_Sum_Equals_K.java
+++ src/com/company/lc_560_Subarray_Sum_Equals_K.java
@@ -7,6 +7,7 @@ public class lc_560_Subarray_Sum_Equals_K {
     public static int subarraySum(int[] nums, int k) {
         int count = 0, sum = 0;
         HashMap < Integer, Integer > map = new HashMap < > ();
+        // purpose to map.put(0, 1) is including equal condition in counter such as 2 - 1 - 1 = 0, then counter 1
         map.put(0, 1);
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
