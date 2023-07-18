eb7576c5ecd62e30b54c5b4967a15826b1600b38
Elain Hu <besthynlove@gmail.com>
2023-07-18 00:22:03 -0400

    m

--- src/com/company/lc_349_Intersection_of_Two_Arrays.java
+++ src/com/company/lc_349_Intersection_of_Two_Arrays.java
@@ -5,23 +5,24 @@ import java.util.*;
 
 public class lc_349_Intersection_of_Two_Arrays {
     public static int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> set = new HashSet();
-        HashSet<Integer> res = new HashSet();
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
 
-        for(int itr = 0; itr < nums1.length; itr++){
-            set.add(nums1[itr]);
+        for (int i = 0; i < nums1.length; i++){
+            set1.add(nums1[i]);
         }
 
-        for(int itr = 0; itr < nums2.length; itr++){
-            if(set.contains(nums2[itr])) res.add(nums2[itr]);
+        for (int j = 0; j < nums2.length; j++){
+            if (set1.contains(nums2[j])) set2.add(nums2[j]);
         }
 
-        int[] arr = new int[res.size()];
-        int jtr = 0;
-        for (int itr : res)
-            arr[jtr++] = itr;
+        int[] result = new int[set2.size()];
+        int index = 0;
+        for (int num : set2){
+            result[index++] = num;
 
-        return arr;
+        }
+        return result;
     }
 
 
