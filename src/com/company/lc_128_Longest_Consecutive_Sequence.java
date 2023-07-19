4e59191dc1c7d69a4f01c931de98b4703e1c870b
Elain Hu <besthynlove@gmail.com>
2023-07-18 23:47:22 -0400

    m

--- src/com/company/lc_128_Longest_Consecutive_Sequence.java
+++ src/com/company/lc_128_Longest_Consecutive_Sequence.java
@@ -3,23 +3,27 @@ import java.util.*;
 
 public class lc_128_Longest_Consecutive_Sequence {
     public static int longestConsecutive(int[] nums) {
-        Set<Integer> num_set = new HashSet<Integer>();
-        for (int num : nums) {
-            num_set.add(num);
-        }
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) set.add(n);
 
-        int longest = 0;
+        int longestStreak = 0;
 
-        for (int num : num_set) {
-            if (!num_set.contains(num-1)) {
-                int length = 0;
-                while (num_set.contains(length + num)) {
-                    length++;
+        for (int num : set) {
+
+            if (!set.contains(num - 1)) {
+                int currNum = num;
+                int currStreak = 1;
+
+                while (set.contains(currNum + 1)) {
+                    currNum++;
+                    currStreak++;
                 }
-                longest = Math.max(longest, length);
+
+                longestStreak = Math.max(longestStreak, currStreak);
             }
         }
-        return longest;
+
+        return longestStreak;
     }
     public static void main(String[] args){
         int[] intArray = new int[]{1,8,6,2,5,4,8,3,7};
