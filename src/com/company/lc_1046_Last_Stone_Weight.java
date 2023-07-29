f8582cd444408a15280f60b367546c49ba769b08
Elain Hu <besthynlove@gmail.com>
2023-07-29 17:33:48 -0400

    m

--- src/com/company/lc_1046_Last_Stone_Weight.java
+++ src/com/company/lc_1046_Last_Stone_Weight.java
@@ -10,10 +10,10 @@ public class lc_1046_Last_Stone_Weight {
         //setting decreasing order of the heap or max heap
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
         for(int st: stones) pq.add(st);
-        while(pq.size() > 1) {
-            int stone1 = pq.poll();
-            int stone2 = pq.poll();
-            if(stone1 > stone2) pq.add(stone1-stone2);
+            while(pq.size() > 1) {
+                int stone1 = pq.poll();
+                int stone2 = pq.poll();
+                if(stone1 > stone2) pq.add(stone1-stone2);
         }
         return pq.isEmpty() ? 0 : pq.peek();
     }
