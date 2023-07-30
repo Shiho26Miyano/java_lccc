93bdbfc20e29c65174f9e72d38a7741938d0a10b
Elain Hu <besthynlove@gmail.com>
2023-07-30 18:32:10 -0400

    m

--- src/com/company/lc_215_Kth_Largest_Element_in_an_Array.java
+++ src/com/company/lc_215_Kth_Largest_Element_in_an_Array.java
@@ -2,7 +2,9 @@ package com.company;
 import java.util.*;
 
 public class lc_215_Kth_Largest_Element_in_an_Array {
+    //method 1 minHeap
     public static int findKthLargest(int[] nums, int k) {
+        //minHeap
         // init heap 'the smallest element first'
         PriorityQueue<Integer> heap =
                 new PriorityQueue<Integer>((n1, n2) -> n1 - n2);
@@ -16,6 +18,17 @@ public class lc_215_Kth_Largest_Element_in_an_Array {
 
         // output
         return heap.poll();
+    }
+    // method 2 maxHeap
+    public static int findKthLargest_2(int[] nums, int k) {
+        PriorityQueue<Integer>pq = new PriorityQueue<>((a,b)->b-a);
+        for (int n : nums) pq.add(n);
+        int totalSize = pq.size();
+        while(totalSize - pq.size() < k - 1){
+            pq.poll();
+        }
+        return pq.peek();
+
     }
 
 
