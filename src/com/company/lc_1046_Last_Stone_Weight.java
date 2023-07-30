e22b66137102de1d69b8010b12435443b949d22b
Elain Hu <besthynlove@gmail.com>
2023-07-30 17:53:06 -0400

    m

--- src/com/company/lc_1046_Last_Stone_Weight.java
+++ src/com/company/lc_1046_Last_Stone_Weight.java
@@ -3,17 +3,17 @@ import java.util.*;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-
+// understand each tips then 5 times to memorize the trick
 public class lc_1046_Last_Stone_Weight {
     public static int lastStoneWeight(int[] stones) {
         //PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         //setting decreasing order of the heap or max heap
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
         for(int st: stones) pq.add(st);
-            while(pq.size() > 1) {
-                int stone1 = pq.poll();
-                int stone2 = pq.poll();
-                if(stone1 > stone2) pq.add(stone1-stone2);
+        while(pq.size() > 1) {
+            int stone1 = pq.poll();
+            int stone2 = pq.poll();
+            if(stone1 > stone2) pq.add(stone1-stone2);
         }
         return pq.isEmpty() ? 0 : pq.peek();
     }
