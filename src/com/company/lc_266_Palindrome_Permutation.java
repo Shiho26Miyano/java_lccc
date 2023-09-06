4b7e44322f448ccb2ad8be3a3944b173caf0333f
Yinan Hu <yinanhu@Yinans-MacBook-Pro.local>
2023-09-06 00:38:19 -0400

    m

--- src/com/company/lc_266_Palindrome_Permutation.java
+++ src/com/company/lc_266_Palindrome_Permutation.java
@@ -1,6 +1,7 @@
 package com.company;
 import java.util.*;
 public class lc_266_Palindrome_Permutation {
+    // solution 1: array
     public static boolean canPermutePalindrome(String s) {
         if (s == null || s.length() == 0) {
             return false;
@@ -17,6 +18,17 @@ public class lc_266_Palindrome_Permutation {
         }
         return count <= 1;
     }
+    // solution 2: hashmap
+    public boolean canPermutePalindrome2(String s) {
+        Map<Character, Integer> map = new HashMap();
+        for(char c : s.toCharArray()){
+            if(map.containsKey(c)) map.remove(c);
+            else map.put(c, 1);
+        }
+        if(map.size() > 1) return false;
+        else return true;
+    }
+
     public static void main(String[] args){
         int[] intArray = new int[]{2,3,2};
         int target = 5;
