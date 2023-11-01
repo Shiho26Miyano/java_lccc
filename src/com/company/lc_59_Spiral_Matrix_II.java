367de13e00dff577e0811f6c3181a113f387436a
Yinan Hu <besthynlove@gmail.com>
2023-10-31 23:50:26 -0400

    m

---
+++ src/com/company/lc_59_Spiral_Matrix_II.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class lc_59_Spiral_Matrix_II {
+    public int[][] generateMatrix(int n) {
+        int startx = 0;
+        int starty = 0;
+        int loop = n / 2;
+        int mid = n / 2;
+        int counter = 1;
+        int cut = 1;
+        int i, j;
+        int[][] res = new int[n][n];
+
+        while(loop-- > 0){
+            for (j = starty; j < n - cut; j++)
+                res[startx][j] = counter++;
+            for (i = startx; i < n - cut; i++)
+                res[i][j] = counter++;
+            for (j = n - cut; j > starty; j--)
+                res[i][j] = counter++;
+            for (i = n - cut; i > startx; i--)
+                res[i][j] = counter++;
+
+            startx++;
+            starty++;
+            cut++;
+        }
+        if (n % 2 != 0 ) {
+            res[mid][mid] = counter;
+        }
+        return res;
+
+    }
+
+}
