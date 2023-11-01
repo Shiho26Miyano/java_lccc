940f18d25efe26896edfe50ade69935983a253a9
Yinan Hu <besthynlove@gmail.com>
2023-10-31 22:53:32 -0400

    m

--- src/com/company/lc_209_Minimum_Size_Subarray_Sum.java
+++ src/com/company/lc_209_Minimum_Size_Subarray_Sum.java
@@ -4,22 +4,21 @@ public class lc_209_Minimum_Size_Subarray_Sum {
     public int minSubArrayLen(int target, int[] nums) {
         if (nums == null || nums.length == 0)
             return 0;
-
         int start = 0;
         int sum = 0;
         int minLength = Integer.MAX_VALUE;
-
-        for (int end = 0; end < nums.length; end++) {
-            sum = sum + nums[end];
-            while (sum >= target) {
-                minLength = end - start + 1 < minLength ? end - start + 1 : minLength;
-                sum = sum - nums[start];
+        for (int end = 0; end < nums.length; end++ ){
+            sum += nums[end];
+            while(sum >= target){
+                int subLength = end - start + 1;
+                minLength = minLength > subLength ? subLength : minLength;
+                sum -= nums[start];
                 start++;
             }
 
+
         }
         return minLength == Integer.MAX_VALUE ? 0 : minLength;
-
 
     }
 }
