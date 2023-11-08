07f49fb32c21f772a30e370bae0e8e94143ccb59
Yinan Hu <besthynlove@gmail.com>
2023-11-07 23:21:21 -0500

    m

--- src/com/company/lc_424_Longest_Repeating_Character_Replacement.java
+++ src/com/company/lc_424_Longest_Repeating_Character_Replacement.java
@@ -6,26 +6,24 @@ import java.util.*;
 //That is, we do not need the accurate max count of the current window; we only care if the max count exceeds the //historical max count; and that can only happen because of the new char.
 public class lc_424_Longest_Repeating_Character_Replacement {
     public static int characterReplacement(String s, int k) {
-        //To record current max repeated counts of character
         int maxRepeat = 0;
-        int start = 0;
+        int startIndex = 0;
         int longest = 0;
-        Map<Character, Integer> charMap = new HashMap<>();
-        for(int end = 0; end < s.length(); end++){
-            char right = s.charAt(end);
-            charMap.put(right, charMap.getOrDefault(right, 0) + 1);
-            //Update the maxRepeat counts
-            maxRepeat = Math.max(maxRepeat, charMap.get(right));
-            while (end - start + 1 - maxRepeat > k){
-                char left = s.charAt(start);
-                charMap.put(left, charMap.get(left) - 1);
-                if(charMap.get(left) == 0){
-                    charMap.remove(left);
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int endIndex = 0; endIndex < s.length(); endIndex++){
+            char end = s.charAt(endIndex);
+            map.put(end, map.getOrDefault(end, 0) + 1);
+            maxRepeat = Math.max(maxRepeat, map.get(end));
+            while(endIndex - startIndex + 1 - maxRepeat > k){
+                char start = s.charAt(startIndex);
+                map.put(start, map.getOrDefault(start, 0) - 1);
+                if (map.get(start) == 0){
+                    map.remove(start);
                 }
-                start++;
+                startIndex++;
             }
-            //Update the longest substring
-            longest = Math.max(longest, end - start + 1);
+            longest = Math.max(longest, endIndex - startIndex + 1);
         }
         return longest;
     }
