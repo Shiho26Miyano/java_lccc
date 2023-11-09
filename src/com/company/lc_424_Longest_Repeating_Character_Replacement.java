566315ed4f080e622626df0d56b0ce5751793f85
Yinan Hu <besthynlove@gmail.com>
2023-11-08 23:30:58 -0500

    m

--- src/com/company/lc_424_Longest_Repeating_Character_Replacement.java
+++ src/com/company/lc_424_Longest_Repeating_Character_Replacement.java
@@ -2,7 +2,6 @@ package com.company;
 import java.util.*;
 //Since we are only interested in the longest valid substring, our sliding windows need not shrink, even if a window may cover an invalid substring. We either grow the window by appending one char on the right, or shift the whole window to the right by one.
 //we only grow the window when the count of the new char exceeds the historical max count (from a previous window that covers a valid substring).
-
 //That is, we do not need the accurate max count of the current window; we only care if the max count exceeds the //historical max count; and that can only happen because of the new char.
 public class lc_424_Longest_Repeating_Character_Replacement {
     public static int characterReplacement(String s, int k) {
