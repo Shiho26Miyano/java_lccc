07cb72cdb9884f830878e0051c64dec9f514a2b6
Yinan Hu <besthynlove@gmail.com>
2023-12-12 06:28:53 -0500

    m

--- src/com/company/lc_424_Longest_Repeating_Character_Replacement.java
+++ src/com/company/lc_424_Longest_Repeating_Character_Replacement.java
@@ -5,7 +5,7 @@ import java.util.*;
 //That is, we do not need the accurate max count of the current window; we only care if the max count exceeds the //historical max count; and that can only happen because of the new char.
 public class lc_424_Longest_Repeating_Character_Replacement {
     public static int characterReplacement(String s, int k) {
-        int maxRepeat = 0;
+        int realRepeat = 0;
         int startIndex = 0;
         int longest = 0;
 
@@ -13,8 +13,8 @@ public class lc_424_Longest_Repeating_Character_Replacement {
         for (int endIndex = 0; endIndex < s.length(); endIndex++){
             char end = s.charAt(endIndex);
             map.put(end, map.getOrDefault(end, 0) + 1);
-            maxRepeat = Math.max(maxRepeat, map.get(end));
-            while(endIndex - startIndex + 1 - maxRepeat > k){
+            realRepeat = Math.max(realRepeat, map.get(end));
+            while(endIndex - startIndex + 1 - realRepeat > k){
                 char start = s.charAt(startIndex);
                 map.put(start, map.getOrDefault(start, 0) - 1);
                 if (map.get(start) == 0){
