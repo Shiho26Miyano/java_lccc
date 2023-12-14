413761ddc1e514bf81ba5adf0dd90a5df32f85f7
Yinan Hu <besthynlove@gmail.com>
2023-12-13 22:06:06 -0500

    m

--- src/com/company/lc_340_Longest_Substring_with_At_Most_K_Distinct_Characters.java
+++ src/com/company/lc_340_Longest_Substring_with_At_Most_K_Distinct_Characters.java
@@ -3,24 +3,27 @@ import java.util.*;
 
 public class lc_340_Longest_Substring_with_At_Most_K_Distinct_Characters {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int startIndex = 0;
+        int mostChar = 0;
+
+
         Map<Character, Integer> map = new HashMap<>();
-        int left = 0;
-        int best = 0;
-        for(int i = 0; i < s.length(); i++) {
-            // character at the right pointer
-            char c = s.charAt(i);
-            map.put(c, map.getOrDefault(c, 0) + 1);
-            // make sure map size is valid, no need to check left pointer less than s.length()
-            while (map.size() > k) {
-                char leftChar = s.charAt(left);
-                map.put(leftChar, map.get(leftChar) - 1);
-                if (map.get(leftChar) == 0) {
-                    map.remove(leftChar);
+
+        for (int endIndex = 0; endIndex < s.length(); endIndex++){
+            char end = s.charAt(endIndex);
+
+            map.put(end, map.getOrDefault(end, 0) + 1);
+
+            while(map.size() > k){
+                char start = s.charAt(startIndex);
+                map.put(start, map.getOrDefault(start, 0) - 1);
+                if (map.get(start) == 0){
+                    map.remove(start);
                 }
-                left++;
+                startIndex++;
             }
-            best = Math.max(best, i - left + 1);
+            mostChar = Math.max(mostChar, endIndex - startIndex + 1);
         }
-        return best;
+        return mostChar;
     }
 }
