dd3e1d42b126cf9474cf21765235b86dc6449103
Yinan Hu <besthynlove@gmail.com>
2024-01-01 19:00:51 -0500

    m

--- src/com/company/lc_3_Longest_Substring_Without_Repeating_Characters.java
+++ src/com/company/lc_3_Longest_Substring_Without_Repeating_Characters.java
@@ -2,7 +2,30 @@ package com.company;
 import java.util.*;
 
 public class lc_3_Longest_Substring_Without_Repeating_Characters {
-    public static int lengthOfLongestSubstring(String s) {
+    // method 1, put {end: count} before cleaning, then update
+    public int lengthOfLongestSubstring(String s) {
+        int startIndex = 0;
+        int longest = 0;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int endIndex = 0; endIndex < s.length(); endIndex++){
+            char end = s.charAt(endIndex);
+
+            map.put(end, map.getOrDefault(end, 0) + 1);
+            while(map.get(end) > 1){
+                char start = s.charAt(startIndex);
+                map.put(start, map.getOrDefault(start, 0) - 1);
+                if (map.get(start) == 0){
+                    map.remove(start);
+                }
+                startIndex++;
+            }
+            longest = Math.max(longest, endIndex - startIndex + 1);
+        }
+        return longest;
+    }
+    // method 2: first cleaning, then put {end: count} in map
+    public static int lengthOfLongestSubstring2(String s) {
         int startIndex = 0;
         int longest = 0;
 
