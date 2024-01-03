498e5cd08aea238cb015fad46a27ce91b9c80c8b
Yinan Hu <besthynlove@gmail.com>
2024-01-02 22:24:47 -0500

    m

--- src/com/company/lc_992_Subarrays_with_K_Different_Integers.java
+++ src/com/company/lc_992_Subarrays_with_K_Different_Integers.java
@@ -3,39 +3,33 @@ import java.util.*;
 
 public class lc_992_Subarrays_with_K_Different_Integers {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return helper(nums, k) - helper(nums, k - 1);
-
-
+        return atMost(nums, k) - atMost(nums, k-1);
     }
+    int atMost(int[] nums, int k){
 
-    public int helper(int[] nums, int k) {
         int startIndex = 0;
-        int count = 0;
+        int res = 0;
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int endIndex = 0; endIndex < nums.length; endIndex++){
-
-
             int end = nums[endIndex];
-
             map.put(end, map.getOrDefault(end, 0) + 1);
 
-            while (map.size() > k) {
+            while(map.size() > k){
                 int start = nums[startIndex];
                 map.put(start, map.getOrDefault(start, 0) - 1);
                 if (map.get(start) == 0){
                     map.remove(start);
                 }
                 startIndex++;
-
             }
-            count += endIndex - startIndex + 1;
+            res += endIndex - startIndex + 1;
 
 
         }
-        return count  ;
-
+        return res;
 
     }
+
 }
