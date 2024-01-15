9f0c9c7d7dc9ba1d4ae183e12b4b64ed31605ebf
Yinan Hu <besthynlove@gmail.com>
2024-01-15 17:22:57 -0500

    m

--- src/com/company/lc_2537_Count_the_Number_of_Good_Subarrays.java
+++ src/com/company/lc_2537_Count_the_Number_of_Good_Subarrays.java
@@ -2,15 +2,15 @@ package com.company;
 import java.util.*;
 public class lc_2537_Count_the_Number_of_Good_Subarrays {
     public long countGood(int[] A, int k) {
-        long res = 0L;
+        long res = 0;
+        int pair = 0;
         Map<Integer, Integer> count = new HashMap<>();
         for(int i = 0, j = 0; j < A.length; ++j){
-            k = k - count.getOrDefault(A[j],0);
+            pair += count.getOrDefault(A[j],0);
             count.put(A[j],count.getOrDefault(A[j],0)+1);
-            while(k <= 0){
+            while(pair >= k){
                 count.put(A[i],count.get(A[i])-1);
-                k = k + count.get(A[i]);
-                i++;
+                pair = pair - count.get(A[i++]);
             }
             res += i;
         }
