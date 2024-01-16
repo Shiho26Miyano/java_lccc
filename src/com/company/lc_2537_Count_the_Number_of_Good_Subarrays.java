8bdc72deb51ac8d531e8df778222e84fa443621f
Yinan Hu <besthynlove@gmail.com>
2024-01-15 20:20:15 -0500

    m

--- src/com/company/lc_2537_Count_the_Number_of_Good_Subarrays.java
+++ src/com/company/lc_2537_Count_the_Number_of_Good_Subarrays.java
@@ -1,21 +1,35 @@
 package com.company;
 import java.util.*;
 public class lc_2537_Count_the_Number_of_Good_Subarrays {
-    public long countGood(int[] nums, int k) {
+    public long countGood(int[] A, int k) {
+        long res = 0;
+        int pair = 0;
+        Map<Integer, Integer> count = new HashMap<>();
+        for(int i = 0, j = 0; j < A.length; ++j){
+            pair += count.getOrDefault(A[j],0);
+            count.put(A[j],count.getOrDefault(A[j],0)+1);
+            while(pair >= k){
+                count.put(A[i],count.get(A[i])-1);
+                pair = pair - count.get(A[i++]);
+            }
+            res += i;
+        }
+        return res;
+    }
+    public long countGoodMethod2(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap();
-        int start = 0;
+
         long ans = 0;
         int count = 0;
-        for (int i=0; i<nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            count += map.getOrDefault(nums[i], 0) - 1;
+        for (int i=0, j = 0; j<nums.length; j++) {
+            count += map.getOrDefault(nums[j], 0);
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+
             while (count >= k) {
-                ans+= nums.length - i;
+                ans+= nums.length - j;
+                map.put(nums[i],map.get(nums[i])-1);
 
-                count -= map.get(nums[start]) - 1;
-
-                map.put(nums[start], map.get(nums[start]) - 1);
-                start++;
+                count -= map.get(nums[i++]);
             }
         }
         return ans;
